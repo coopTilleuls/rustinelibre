8f5aef59304587ed5228fa5e5615be0be83b55bf
clementtalleu <clement.talleu@gmail.com>
2023-03-21 17:14:19 +0100

    add meilisearch docker + bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Meilisearch\Bundle\MeilisearchBundle::class => ['all' => true],
 ];
