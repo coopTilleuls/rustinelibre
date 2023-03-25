7c0ebc79014d4aab0fcb9f2fef894b0dabfe2288
clementtalleu <clement.talleu@gmail.com>
2023-03-25 20:39:15 +0100

    resume all in one migration

--- api/migrations/Version20230325193849.php
+++ api/migrations/Version20230325193849.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230323092640 extends AbstractMigration
+final class Version20230325193849 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,19 +21,26 @@ final class Version20230323092640 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE appointment_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE bike_type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE repairer_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE appointment (id INT NOT NULL, customer_id INT DEFAULT NULL, repairer_id INT NOT NULL, slot_time TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, duration INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_FE38F8449395C3F3 ON appointment (customer_id)');
         $this->addSql('CREATE INDEX IDX_FE38F84447C5DFEE ON appointment (repairer_id)');
         $this->addSql('COMMENT ON COLUMN appointment.slot_time IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE repairer (id INT NOT NULL, owner_id INT NOT NULL, description TEXT DEFAULT NULL, mobile_phone VARCHAR(255) DEFAULT NULL, street VARCHAR(800) DEFAULT NULL, city VARCHAR(255) NOT NULL, postcode VARCHAR(255) DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, rrule VARCHAR(255) NOT NULL, latitude VARCHAR(255) DEFAULT NULL, longitude VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE bike_type (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE repairer (id INT NOT NULL, owner_id INT NOT NULL, name VARCHAR(255) DEFAULT NULL, description TEXT DEFAULT NULL, mobile_phone VARCHAR(255) DEFAULT NULL, street VARCHAR(800) DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, postcode VARCHAR(255) DEFAULT NULL, country VARCHAR(255) DEFAULT NULL, rrule VARCHAR(255) DEFAULT NULL, latitude VARCHAR(255) DEFAULT NULL, longitude VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_4A73F2BF7E3C61F9 ON repairer (owner_id)');
+        $this->addSql('CREATE TABLE repairer_bike_type (repairer_id INT NOT NULL, bike_type_id INT NOT NULL, PRIMARY KEY(repairer_id, bike_type_id))');
+        $this->addSql('CREATE INDEX IDX_D5AC763447C5DFEE ON repairer_bike_type (repairer_id)');
+        $this->addSql('CREATE INDEX IDX_D5AC76347FF015AE ON repairer_bike_type (bike_type_id)');
         $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, email_confirmed BOOLEAN NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
         $this->addSql('ALTER TABLE appointment ADD CONSTRAINT FK_FE38F8449395C3F3 FOREIGN KEY (customer_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE appointment ADD CONSTRAINT FK_FE38F84447C5DFEE FOREIGN KEY (repairer_id) REFERENCES repairer (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE repairer ADD CONSTRAINT FK_4A73F2BF7E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE repairer_bike_type ADD CONSTRAINT FK_D5AC763447C5DFEE FOREIGN KEY (repairer_id) REFERENCES repairer (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE repairer_bike_type ADD CONSTRAINT FK_D5AC76347FF015AE FOREIGN KEY (bike_type_id) REFERENCES bike_type (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
@@ -41,13 +48,18 @@ final class Version20230323092640 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE appointment_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE bike_type_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE repairer_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
         $this->addSql('ALTER TABLE appointment DROP CONSTRAINT FK_FE38F8449395C3F3');
         $this->addSql('ALTER TABLE appointment DROP CONSTRAINT FK_FE38F84447C5DFEE');
         $this->addSql('ALTER TABLE repairer DROP CONSTRAINT FK_4A73F2BF7E3C61F9');
+        $this->addSql('ALTER TABLE repairer_bike_type DROP CONSTRAINT FK_D5AC763447C5DFEE');
+        $this->addSql('ALTER TABLE repairer_bike_type DROP CONSTRAINT FK_D5AC76347FF015AE');
         $this->addSql('DROP TABLE appointment');
+        $this->addSql('DROP TABLE bike_type');
         $this->addSql('DROP TABLE repairer');
+        $this->addSql('DROP TABLE repairer_bike_type');
         $this->addSql('DROP TABLE "user"');
     }
 }
