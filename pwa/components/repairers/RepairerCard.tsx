95287996b57cc79c9f549742e74dba742e08687c
clementtalleu <clement.talleu@gmail.com>
2023-03-27 18:36:56 +0200

    add formatDate helper

--- pwa/components/repairers/RepairerCard.tsx
+++ pwa/components/repairers/RepairerCard.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 import React from "react";
 import {Repairer} from "../../interfaces/Repairer";
+import {formatDate} from 'helpers/dateHelper';
 
 interface RepairerProps {
     repairer: Repairer;
@@ -8,10 +8,11 @@ interface RepairerProps {
 }
 
 export const RepairerCard = ({repairer, isSelect}: RepairerProps): JSX.Element => {
+
     return (
         <div className={`max-w-sm rounded overflow-hidden shadow-lg mb-10 ${isSelect && 'bg-green-400'}`}>
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">Un réparateur </div>
+                <div className="font-bold text-xl mb-2">{repairer.name}</div>
                 <p className="text-gray-700 text-base">
                     {repairer.street}  <br/>
                     {repairer.postcode} {repairer.city}
@@ -20,8 +21,7 @@ export const RepairerCard = ({repairer, isSelect}: RepairerProps): JSX.Element =
                     repairer.firstSlotAvailable !== undefined ?
                     <p>
                         <strong>Prochaine disponibilité : </strong>
-                        {new Date(repairer.firstSlotAvailable.date).toLocaleDateString()} -
-                        {new Date(repairer.firstSlotAvailable.date).toLocaleTimeString()}
+                        {formatDate(repairer.firstSlotAvailable.date)}
                     </p> : 'Pas de créneau indiqué'
                 }
             </div>
