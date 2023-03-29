c4296e9f53693a66e205c144f6dbaefc17153f26
clementtalleu <clement.talleu@gmail.com>
2023-03-29 16:49:36 +0200

    remove create extension from ci

--- api/migrations/Version20230328140424.php
+++ api/migrations/Version20230328140424.php
@@ -16,7 +16,7 @@ final class Version20230328140424 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE EXTENSION Postgis;');
+        $this->addSql('CREATE EXTENSION Postgis');
     }
 
     public function down(Schema $schema): void
