d0ee8f3614f898022d02df3d008ab5165b52ee14
clementtalleu <clement.talleu@gmail.com>
2023-03-30 15:12:02 +0200

    add repairer types

---
+++ api/src/Entity/RepairerType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use App\Repository\RepairerTypeRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: RepairerTypeRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['repairer_type_read']],
+    denormalizationContext: ['groups' => ['repairer_type_write']],
+)]
+#[Get]
+#[GetCollection]
+#[Put(security: "is_granted('ROLE_ADMIN')")]
+#[Post(security: "is_granted('ROLE_ADMIN')")]
+class RepairerType
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['repairer_type_read', 'repairer_read'])]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['repairer_type_read', 'repairer_read', 'repairer_type_write'])]
+    private ?string $name = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
---
+++ api/tests/Repairer/Filters/SearchFilterTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Repairer\Filters;
+
+use App\Tests\AbstractTestCase;
+
+class SearchFilterTest extends AbstractTestCase
+{
+    public function testSearchFilter(): void
+    {
+        $response = static::createClient()->request('GET', '/repairers?repairerType.name=atelier');
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('content-type', 'application/ld+json; charset=utf-8');
+
+        $responseData = $response->toArray();
+        $this->assertEquals(2, $responseData['hydra:totalItems']);
+        $this->assertStringContainsString('atelier', strtolower($responseData['hydra:member'][0]['repairerType']['name']));
+        $this->assertStringContainsString('atelier', strtolower($responseData['hydra:member'][1]['repairerType']['name']));
+    }
+}
