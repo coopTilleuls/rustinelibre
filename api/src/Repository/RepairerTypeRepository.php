b909e415114abf4a15f7e31cd987bac8edd0027f
clementtalleu <clement.talleu@gmail.com>
2023-03-30 15:13:57 +0200

    remove useless comm

--- api/src/Repository/RepairerTypeRepository.php
+++ api/src/Repository/RepairerTypeRepository.php
@@ -38,29 +38,4 @@ class RepairerTypeRepository extends ServiceEntityRepository
             $this->getEntityManager()->flush();
         }
     }
-
-//    /**
-//     * @return RepairerType[] Returns an array of RepairerType objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('r.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?RepairerType
-//    {
-//        return $this->createQueryBuilder('r')
-//            ->andWhere('r.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
