e5eb14333f29255d565aad8d6ecb291f2298c62d
Raphael <marchese.raphael@gmail.com>
2023-03-31 15:38:15 +0200

    Entity fixed

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -20,6 +20,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+
 #[ApiResource(
     normalizationContext: ['groups' => ['user_read']],
     denormalizationContext: ['groups' => ['user_write']]
