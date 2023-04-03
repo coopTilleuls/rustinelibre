b711af4d062f69e78cdbc16a3e972e6963baadb6
Raphael <marchese.raphael@gmail.com>
2023-04-03 11:52:04 +0200

    wip proximity filter

---
+++ api/src/Repairers/Filter/ProximityFilter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Repairers\Filter;
+
+use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Component\PropertyInfo\Type;
+
+class ProximityFilter extends AbstractFilter
+{
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
+    {
+        // otherwise filter is applied to order and page as well
+        if (
+            !$this->isPropertyEnabled($property, $resourceClass) ||
+            !$this->isPropertyMapped($property, $resourceClass)
+        ) {
+            return;
+        }
+
+        $parameterName = $queryNameGenerator->generateParameterName($property); // Generate a unique parameter name to avoid collisions with other filters
+        $queryBuilder
+            ->orderBy( 'ST_Distance_Sphere(o.gpsPoint, ST_GeogFromText(:point))', 'ASC' )
+            ->setParameter('point', sprintf('POINT(%f %f)', $longitude, $latitude),
+    }
+
+    // This function is only used to hook in documentation generators (supported by Swagger and Hydra)
+    public function getDescription(string $resourceClass): array
+    {
+        if (!$this->properties) {
+            return [];
+        }
+
+        $description = [];
+        foreach ($this->properties as $property => $strategy) {
+            $description["regexp_$property"] = [
+                'property' => $property,
+                'type' => Type::BUILTIN_TYPE_STRING,
+                'required' => false,
+                'description' => 'Filter using a regex. This will appear in the OpenApi documentation!',
+                'openapi' => [
+                    'example' => 'Custom example that will be in the documentation and be the default value of the sandbox',
+                    'allowReserved' => false,// if true, query parameters will be not percent-encoded
+                    'allowEmptyValue' => true,
+                    'explode' => false, // to be true, the type must be Type::BUILTIN_TYPE_ARRAY, ?product=blue,green will be ?product=blue&product=green
+                ],
+            ];
+        }
+
+        return $description;
+    }
+}
