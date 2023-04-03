ac9af0b927363655c47462ab46bbb9439e19fe96
clementtalleu <clement.talleu@gmail.com>
2023-04-03 16:08:24 +0200

    fix proximity filter OK needs tests

--- api/src/Repairers/Filter/ProximityFilter.php
+++ api/src/Repairers/Filter/ProximityFilter.php
@@ -1,27 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repairers\Filter;
 
 use ApiPlatform\Doctrine\Orm\Filter\AbstractFilter;
 use ApiPlatform\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use ApiPlatform\Metadata\Operation;
 use Doctrine\ORM\QueryBuilder;
 use Symfony\Component\PropertyInfo\Type;
 
 final class ProximityFilter extends AbstractFilter
 {
     public const PROPERTY_NAME = 'proximity';
+
     protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, Operation $operation = null, array $context = []): void
     {
         if (self::PROPERTY_NAME !== $property) {
             return;
         }
 
-        $coordinates = explode(',', $value[]);
-        $queryBuilder
-            ->orderBy( 'ST_Distance_Sphere(
-            o.gpsPoint, 
-            ST_GeogFromText(:point))', 'ASC' )
-            ->setParameter('point', sprintf('POINT(%f %f)', $coordinates[0], $coordinates[1]));
+        $coordinates = explode(',', $value);
+        $queryBuilder->addSelect(sprintf('ST_Distance(o.gpsPoint, ST_SetSRID(ST_MakePoint(%s, %s), 4326)) as distance', $coordinates[0], $coordinates[1]));
+        $queryBuilder->addOrderBy('distance', 'ASC');
     }
 
     public function getDescription(string $resourceClass): array
@@ -36,12 +37,12 @@ final class ProximityFilter extends AbstractFilter
                 'property' => $property,
                 'type' => Type::BUILTIN_TYPE_STRING,
                 'required' => false,
-                'description' => 'Filter to get data order by proximity',
+                'description' => 'Filter to get data order by proximity from a given latitude/longitude. Default order is ASC',
                 'openapi' => [
-                    'example' => '/repairers?proximity=latitude,longitude&order=asc',
-                    'allowReserved' => false,// if true, query parameters will be not percent-encoded
+                    'example' => '/repairers?proximity=<latitude,longitude>',
+                    'allowReserved' => false,
                     'allowEmptyValue' => true,
-                    'explode' => false, // to be true, the type must be Type::BUILTIN_TYPE_ARRAY, ?product=blue,green will be ?product=blue&product=green
+                    'explode' => false,
                 ],
             ];
         }
