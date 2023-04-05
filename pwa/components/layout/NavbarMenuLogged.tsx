1f097feaa90dfb0e4e7f33f54d65fe70b7d00cc2
clementtalleu <clement.talleu@gmail.com>
2023-04-05 14:23:05 +0200

    add registration page

---
+++ pwa/components/layout/NavbarMenuLogged.tsx
@@ -0,0 +1,35 @@
+export const pagesNotLogged = [
+    {
+        'name': 'Notre collectif',
+        'link': '/',
+    },
+    {
+        'name': 'Inscription',
+        'link': '/inscription',
+    },
+    {
+        'name': 'Liste des réparateurs',
+        'link': '/',
+    },
+    {
+        'name': 'FAQ',
+        'link': '/',
+    },
+    {
+        'name': 'Devenir réparateur',
+        'link': '/',
+    }];
+
+export const pagesLogged = [
+    {
+        'name': 'Notre collectif',
+        'link': '/',
+    },
+    {
+        'name': 'Liste des réparateurs',
+        'link': '/',
+    },
+    {
+        'name': 'FAQ',
+        'link': '/',
+    }];
