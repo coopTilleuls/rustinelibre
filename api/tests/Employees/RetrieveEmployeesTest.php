196122314509b3b00d86e4c1c656ba1f5247c8a0
clementtalleu <clement.talleu@gmail.com>
2023-04-13 10:07:11 +0200

    PR review

--- api/tests/Employees/RetrieveEmployeesTest.php
+++ api/tests/Employees/RetrieveEmployeesTest.php
@@ -22,8 +22,10 @@ class RetrieveEmployeesTest extends AbstractTestCase
 
     public function testGetRepairerEmployeesAsAdmin(): void
     {
-        $this->createClientAuthAsAdmin()->request('GET', '/repairer_employees');
+        $response = $this->createClientAuthAsAdmin()->request('GET', '/repairer_employees');
         $this->assertResponseIsSuccessful();
+        // Minimum 4 employees (as in fixtures), maybe more if other test create it
+        $this->assertTrue(4 <= $response->toArray()['hydra:totalItems']);
     }
 
     public function testGetRepairerEmployeeAsAdmin(): void
@@ -46,36 +48,14 @@ class RetrieveEmployeesTest extends AbstractTestCase
         $this->assertResponseIsSuccessful();
     }
 
-    public function testGetRepairerEmployeesAsBoss(): void
-    {
-        // Boss of repairer 1 try to get an employee of repairer 2
-        $this->createClientAuthAsBoss()->request('GET', '/repairer_employees/3');
-        $this->assertResponseStatusCodeSame(403);
-    }
-
-    public function testGetRepairerEmployeesAsGoodBoss(): void
-    {
-        // Boss of repairer 1 try to get an employee of repairer 1
-        $this->createClientWithCredentials(['email' => 'boss@test.com', 'password' => 'test'])->request('GET', '/repairer_employees/1');
-        $this->assertResponseIsSuccessful();
-    }
-
-    public function testGetRepairersAsAdmin(): void
-    {
-        // Boss of repairer 1 try to get an employee of repairer 2
-        $response = $this->createClientAuthAsAdmin()->request('GET', '/repairer_employees');
-        // Minimum 4 employees (as in fixtures), maybe more if other test create it
-        $this->assertTrue(4 <= $response->toArray()['hydra:totalItems']);
-    }
-
-    public function testGetRepairersAsBossWithEmployees(): void
+    public function testGetEmployeesAsBossWithEmployees(): void
     {
         // Boss of repairer 1 try to get an employee of repairer 2
         $response = $this->createClientAuthAsBoss()->request('GET', '/repairer_employees');
         $this->assertEquals(2, $response->toArray()['hydra:totalItems']);
     }
 
-    public function testGetRepairersAsBossWithNoEmployees(): void
+    public function testGetEmployeesAsBossWithNoEmployees(): void
     {
         // Boss of repairer 1 try to get an employee of repairer 2
         $response = $this->createClientWithUserId(28)->request('GET', '/repairer_employees');
