9a3bb72ff0c34c9f25d0919f0e2e6db8df8bd5a3
clementtalleu <clement.talleu@gmail.com>
2023-04-14 10:06:18 +0200

    Add mailer and send email to new employee  (#174)
    * add sf/mailer and send email to new employee
    
    * remove prio + new lines
    
    * add log
    
    * erase password

---
+++ api/src/Employees/EventSubscriber/EmployeeSendPasswordEventSubscriber.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Employees\EventSubscriber;
+
+use App\Entity\RepairerEmployee;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
+use Doctrine\Persistence\Event\LifecycleEventArgs as BaseLifecycleEventArgs;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
+
+final class EmployeeSendPasswordEventSubscriber implements EventSubscriber
+{
+    public function __construct(private readonly MailerInterface $mailer,
+                                private readonly string $mailerSender,
+                                private readonly string $webAppUrl,
+                                private readonly KernelInterface $kernel,
+                                private readonly LoggerInterface $logger,
+                                private readonly Environment $twig)
+    {
+    }
+
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist,
+        ];
+    }
+
+    public function postPersist(BaseLifecycleEventArgs $args): void
+    {
+        $entity = $args->getObject();
+
+        if (!$entity instanceof RepairerEmployee || in_array($this->kernel->getEnvironment(), ['dev', 'test'])) {
+            return;
+        }
+
+        $userEmployee = $entity->getEmployee();
+        $email = (new Email())
+            ->from($this->mailerSender)
+            ->to($userEmployee->getEmail())
+            ->subject('Votre compte Bikelib vient d\'être créé')
+            ->html($this->twig->render('mail/employee_send_password.html.twig', [
+                'webAppUrl' => $this->webAppUrl,
+                'employee' => $userEmployee,
+                'repairer' => $entity->getRepairer(),
+            ]));
+
+        try {
+            $this->mailer->send($email);
+        } catch (\Exception $e) {
+            $this->logger->alert(sprintf('New employee password email not send, error: %s', $e->getMessage()));
+        }
+
+        $userEmployee->eraseCredentials();
+    }
+}
