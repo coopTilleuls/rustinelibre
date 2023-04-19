6e6188d35534014e53780a12710609f2351cc5e5
Raphael-Marchese <103203222+Raphael-Marchese@users.noreply.github.com>
2023-04-19 16:05:15 +0200

    Delete getters and setters (#200)
    Update appointment entity, set properties on public, adapt tests and event subscriber

--- api/src/Entity/Appointment.php
+++ api/src/Entity/Appointment.php
@@ -51,73 +51,22 @@ class Appointment
     #[ORM\GeneratedValue]
     #[ORM\Column]
     #[Groups([self::APPOINTMENT_READ])]
-    private ?int $id = null;
+    public ?int $id = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(onDelete: 'CASCADE')]
     #[Groups([self::APPOINTMENT_READ, self::APPOINTMENT_WRITE])]
-    private ?User $customer = null;
+    public ?User $customer = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false, onDelete: 'CASCADE')]
     #[Groups([self::APPOINTMENT_READ, self::APPOINTMENT_WRITE])]
-    private ?Repairer $repairer = null;
+    public ?Repairer $repairer = null;
 
     #[ORM\Column]
     #[Groups([self::APPOINTMENT_READ, self::APPOINTMENT_WRITE])]
-    private ?\DateTimeImmutable $slotTime = null;
+    public ?\DateTimeImmutable $slotTime = null;
 
     #[ORM\Column(nullable: true)]
-    private ?int $duration = 60;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function getCustomer(): ?User
-    {
-        return $this->customer;
-    }
-
-    public function setCustomer(?User $customer): self
-    {
-        $this->customer = $customer;
-
-        return $this;
-    }
-
-    public function getRepairer(): ?Repairer
-    {
-        return $this->repairer;
-    }
-
-    public function setRepairer(?Repairer $repairer): void
-    {
-        $this->repairer = $repairer;
-    }
-
-    public function getSlotTime(): ?\DateTimeImmutable
-    {
-        return $this->slotTime;
-    }
-
-    public function setSlotTime(\DateTimeImmutable $slotTime): self
-    {
-        $this->slotTime = $slotTime;
-
-        return $this;
-    }
-
-    public function getDuration(): ?int
-    {
-        return $this->duration;
-    }
-
-    public function setDuration(?int $duration): self
-    {
-        $this->duration = $duration;
-
-        return $this;
-    }
+    public ?int $duration = 60;
 }
