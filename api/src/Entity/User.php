446f0a2ec03938093b96212ee2e1f7ebccdde775
Raphael <marchese.raphael@gmail.com>
2023-04-19 17:03:54 +0200

    back regex same constraints with front regex

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -71,7 +71,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string')]
     public ?string $password = null;
 
-    #[Assert\Regex("/^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])(?=.*[@$!%*?&\\/])[A-Za-z\d@$!%*?&\\/]{12,}$/i")]
+    #[Assert\Regex('/^(?=.*[A-Z])(?=.*\d)(?=.*[a-z])(?=.*[@$!%*#?.+=;\\/:,_"&])[A-Za-z\d@$!%*#?.+=;,\\/:_"&]{12,}$/i')]
     #[Groups([self::USER_READ, self::USER_WRITE])]
     public ?string $plainPassword = null;
 
