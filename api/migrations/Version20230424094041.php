4e76f3be6b8d39827e398250bd3e5fb40b4cc4f6
Davy <davy.beauzil72@gmail.com>
2023-04-24 11:48:53 +0200

    adapt migration and add bike fixtures

--- api/migrations/Version20230424094041.php
+++ api/migrations/Version20230424094041.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20230424085001 extends AbstractMigration
+final class Version20230424094041 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,13 +19,13 @@ final class Version20230424085001 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE bike_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE bike (id INT NOT NULL, owner_id INT NOT NULL, bike_type_id INT DEFAULT NULL, picture_id INT DEFAULT NULL, wheel_picture_id INT DEFAULT NULL, transmission_picture_id INT DEFAULT NULL, brand VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, description TEXT DEFAULT NULL, details TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_4CBC37807E3C61F9 ON bike (owner_id)');
+        $this->addSql('CREATE INDEX IDX_4CBC37807E3C61F9 ON bike (owner_id)');
         $this->addSql('CREATE INDEX IDX_4CBC37807FF015AE ON bike (bike_type_id)');
         $this->addSql('CREATE INDEX IDX_4CBC3780EE45BDBF ON bike (picture_id)');
         $this->addSql('CREATE INDEX IDX_4CBC378093E0DCF6 ON bike (wheel_picture_id)');
         $this->addSql('CREATE INDEX IDX_4CBC37806B1E0CCD ON bike (transmission_picture_id)');
         $this->addSql('COMMENT ON COLUMN bike.created_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE bike ADD CONSTRAINT FK_4CBC37807E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE bike ADD CONSTRAINT FK_4CBC37807E3C61F9 FOREIGN KEY (owner_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE bike ADD CONSTRAINT FK_4CBC37807FF015AE FOREIGN KEY (bike_type_id) REFERENCES bike_type (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE bike ADD CONSTRAINT FK_4CBC3780EE45BDBF FOREIGN KEY (picture_id) REFERENCES media_object (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE bike ADD CONSTRAINT FK_4CBC378093E0DCF6 FOREIGN KEY (wheel_picture_id) REFERENCES media_object (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
