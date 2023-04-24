8b298fab98e55738dfd8c76d43910e9acd53b611
Davy <davy.beauzil72@gmail.com>
2023-04-24 14:42:18 +0200

    remove details from bike

--- api/migrations/Version20230424124031.php
+++ api/migrations/Version20230424124031.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20230424094041 extends AbstractMigration
+final class Version20230424124031 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -18,7 +18,7 @@ final class Version20230424094041 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE bike_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE bike (id INT NOT NULL, owner_id INT NOT NULL, bike_type_id INT DEFAULT NULL, picture_id INT DEFAULT NULL, wheel_picture_id INT DEFAULT NULL, transmission_picture_id INT DEFAULT NULL, brand VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, description TEXT DEFAULT NULL, details TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE bike (id INT NOT NULL, owner_id INT NOT NULL, bike_type_id INT DEFAULT NULL, picture_id INT DEFAULT NULL, wheel_picture_id INT DEFAULT NULL, transmission_picture_id INT DEFAULT NULL, brand VARCHAR(255) DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, description TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_4CBC37807E3C61F9 ON bike (owner_id)');
         $this->addSql('CREATE INDEX IDX_4CBC37807FF015AE ON bike (bike_type_id)');
         $this->addSql('CREATE INDEX IDX_4CBC3780EE45BDBF ON bike (picture_id)');
