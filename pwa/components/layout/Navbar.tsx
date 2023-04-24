90f70e6c969130617c4a5051a7995aba4803fdcc
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-04-24 16:47:27 +0200

    improve navbar

--- pwa/components/layout/Navbar.tsx
+++ pwa/components/layout/Navbar.tsx
@@ -1,179 +1,77 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
+import {useRouter} from 'next/router';
+import {useAuth} from '@contexts/AuthContext';
+import {pagesLogged} from '@components/layout/NavbarMenuLogged';
+import {pagesNotLogged} from '@components/layout/NavbarMenuLogged';
+import NavbarDesktop from '@components/navbar/NavbarDesktop';
+import MobileNavbar from '@components/navbar/NavbarMobile';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
-import {pagesLogged} from "@components/layout/NavbarMenuLogged";
-import {pagesNotLogged} from "@components/layout/NavbarMenuLogged";
-import {User} from "@interfaces/User";
-import {getRoles} from "@helpers/sessionHelper";
-import {useEffect, useState} from "react";
-import {useAuth} from "@contexts/AuthContext";
+import {getRoles} from '@helpers/sessionHelper';
+import {User} from '@interfaces/User';
 
 interface NavbarProps {
-    user?: User;
+  user?: User;
 }
 
 const Navbar = ({user}: NavbarProps): JSX.Element => {
+  const router = useRouter();
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const pages = user ? pagesLogged : pagesNotLogged;
+  const [boss, setBoss] = useState<boolean>(false);
+  const [employee, setEmployee] = useState<boolean>(false);
+  const [admin, setAdmin] = useState<boolean>(false);
+  const {logout} = useAuth();
 
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-    const pages = user ? pagesLogged : pagesNotLogged;
-    const [boss, setBoss] = useState<boolean>(false);
-    const [employee, setEmployee] = useState<boolean>(false);
-    const [admin, setAdmin] = useState<boolean>(false);
-    const {logout} = useAuth();
+  useEffect(() => {
+    if (user) {
+      const roles = getRoles();
+      roles?.includes('ROLE_ADMIN') ? setAdmin(true) : setAdmin(false);
+      roles?.includes('ROLE_EMPLOYEE') ? setEmployee(true) : setEmployee(false);
+      roles?.includes('ROLE_BOSS') ? setBoss(true) : setBoss(false);
+    }
+  }, [user]);
 
-    useEffect(() => {
-        if (user) {
-            const roles = getRoles();
-            roles?.includes('ROLE_ADMIN') ? setAdmin(true) : setAdmin(false);
-            roles?.includes('ROLE_EMPLOYEE') ? setEmployee(true) : setEmployee(false);
-            roles?.includes('ROLE_BOSS') ? setBoss(true) : setBoss(false);
-        }
-    }, [user]);
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const clickLogOut = () => {
+    logout();
+    router.push('/');
+  };
 
-    const clickLogOut = () => {
-        logout();
-    };
-
-    return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <DirectionsBikeIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href="/"
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'none', md: 'flex' },
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Bikelib
-                    </Typography>
-
-                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                        <MenuIcon />
-                        </IconButton>
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            {pages.map((page) => (
-                                <MenuItem href={page.link} sx={{ textAlign: 'center' }} key={page.name} onClick={handleCloseNavMenu}>
-                                    <Typography textAlign="center">{page.name}</Typography>
-                                </MenuItem>
-                            ))}
-                            {
-                                boss &&  <MenuItem href="/dashboard" sx={{ textAlign: 'center' }} key="dashboard_boss" onClick={handleCloseNavMenu}>
-                                    <Typography textAlign="center">Dashboard</Typography>
-                                </MenuItem>
-                            }
-                            {
-                                user &&  <MenuItem sx={{ textAlign: 'center' }} key="logout" onClick={clickLogOut}>
-                                    <Typography textAlign="center">Déconnexion</Typography>
-                                </MenuItem>
-                            }
-                        </Menu>
-                    </Box>
-                    <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: { xs: 'flex', md: 'none' },
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        Bikelib
-                    </Typography>
-                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                        {pages.map((page) => (
-                            <Button
-                                href={page.link}
-                                key={page.name}
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                {page.name}
-                            </Button>
-                        ))}
-                        {
-                            boss &&  <Button
-                                href="/dashboard"
-                                key="dashboard"
-                                onClick={handleCloseNavMenu}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                Dashboard
-                            </Button>
-                        }
-                        {
-                            user &&  <Button
-                                key="logout"
-                                onClick={clickLogOut}
-                                sx={{ my: 2, color: 'white', display: 'block' }}
-                            >
-                                Déconnexion
-                            </Button>
-                        }
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
-    );
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <NavbarDesktop
+            pages={pages}
+            boss={boss}
+            user={user}
+            handleClose={handleCloseNavMenu}
+            logOut={clickLogOut}
+          />
+          <MobileNavbar
+            pages={pages}
+            boss={boss}
+            user={user}
+            handleClose={handleCloseNavMenu}
+            handleOpen={handleOpenNavMenu}
+            logOut={clickLogOut}
+            anchorElNav={anchorElNav}
+          />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 
 export default Navbar;
---
+++ pwa/components/navbar/NavbarDesktop.tsx
@@ -0,0 +1,126 @@
+import * as React from 'react';
+import Link from 'next/link';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import DirectionsBikeIcon from '@mui/icons-material/DirectionsBike';
+import {Container} from '@mui/material';
+import Box from '@mui/material/Box';
+import {Page} from '@interfaces/Page';
+import {User} from '@interfaces/User';
+
+interface NavbarDesktopProps {
+  pages: Page[];
+  boss: boolean;
+  user?: User;
+  handleClose?: () => void;
+  logOut?: () => void;
+}
+
+const NavbarDesktop = ({
+  pages,
+  boss,
+  user,
+  handleClose,
+  logOut,
+}: NavbarDesktopProps): JSX.Element => {
+  return (
+    <Container
+      sx={{
+        display: {xs: 'none', md: 'flex'},
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        my: 2,
+      }}>
+      <Link href="/" style={{textDecoration: 'none'}}>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            color: 'white',
+          }}>
+          <DirectionsBikeIcon sx={{mr: 1}} fontSize="large" />
+          <Typography
+            sx={{
+              mr: 2,
+              fontSize: 20,
+              fontWeight: 900,
+            }}>
+            Bikelib
+          </Typography>
+        </Box>
+      </Link>
+      <Box sx={{display: 'flex', justifyContent: 'center'}}>
+        {pages.map((page) => {
+          return (
+            <Link
+              key={page.name}
+              href={page.link}
+              style={{textDecoration: 'none'}}>
+              <Button
+                onClick={handleClose}
+                sx={{
+                  display: 'flex',
+                  paddingX: 2,
+                  marginX: 0.5,
+                  textTransform: 'capitalize',
+                  fontSize: 16,
+                  fontWeight: 600,
+                  borderRadius: 20,
+                  color: 'white',
+                  '&:hover': {
+                    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                  },
+                }}>
+                {page.name}
+              </Button>
+            </Link>
+          );
+        })}
+        {boss && user && (
+          <Link href="/dashboard" style={{textDecoration: 'none'}}>
+            <Button
+              key="dashboard"
+              onClick={handleClose}
+              sx={{
+                display: 'flex',
+                paddingX: 2,
+                marginX: 1,
+                textTransform: 'capitalize',
+                fontSize: 16,
+                fontWeight: 600,
+                borderRadius: 20,
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                },
+              }}>
+              Dashboard
+            </Button>
+          </Link>
+        )}
+        {user && (
+          <Button
+            key="logout"
+            onClick={logOut}
+            sx={{
+              display: 'flex',
+              color: 'white',
+              paddingX: 2,
+              marginX: 1,
+              textTransform: 'capitalize',
+              fontSize: 16,
+              fontWeight: 600,
+              borderRadius: 20,
+              '&:hover': {
+                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+              },
+            }}>
+            Déconnexion
+          </Button>
+        )}
+      </Box>
+    </Container>
+  );
+};
+
+export default NavbarDesktop;
