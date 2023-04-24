0277096ae35d674c099fadfac732052c69eaecd9
clementtalleu <clement.talleu@gmail.com>
2023-04-24 09:41:09 +0200

    create bike entity+resource

---
+++ api/src/Entity/Bike.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\BikeRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BikeRepository::class)]
+#[ApiResource]
+class Bike
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    public ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    public ?string $brand = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: true)]
+    public ?BikeType $bikeType = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    public ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    public ?string $description = null;
+
+    #[ORM\Column(nullable: true)]
+    public ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(targetEntity: MediaObject::class)]
+    #[ORM\JoinColumn(nullable: true)]
+    #[ApiProperty(types: ['https://schema.org/image'])]
+    public ?MediaObject $picture = null;
+
+    #[ORM\ManyToOne(targetEntity: MediaObject::class)]
+    #[ORM\JoinColumn(nullable: true)]
+    #[ApiProperty(types: ['https://schema.org/image'])]
+    public ?MediaObject $wheelPicture = null;
+
+    #[ORM\ManyToOne(targetEntity: MediaObject::class)]
+    #[ORM\JoinColumn(nullable: true)]
+    #[ApiProperty(types: ['https://schema.org/image'])]
+    public ?MediaObject $transmissionPicture = null;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+    }
+}
