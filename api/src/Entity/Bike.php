878a0589cfc0532db137ff21da559a3f0ecd39e0
Davy <davy.beauzil72@gmail.com>
2023-04-24 13:45:55 +0200

    rename const and add write on images

--- api/src/Entity/Bike.php
+++ api/src/Entity/Bike.php
@@ -6,6 +6,7 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
 use App\Repository\BikeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
@@ -13,44 +14,44 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: BikeRepository::class)]
 #[ApiResource(
-    normalizationContext: ['groups' => [self::REPAIRER_READ]],
-    denormalizationContext: ['groups' => [self::REPAIRER_WRITE]]
+    normalizationContext: ['groups' => [self::READ]],
+    denormalizationContext: ['groups' => [self::WRITE]]
 )]
 class Bike
 {
-    public const REPAIRER_READ = 'bike_read';
-    public const REPAIRER_WRITE = 'bike_write';
+    public const READ = 'bike_read';
+    public const WRITE = 'bike_write';
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups([self::REPAIRER_READ])]
+    #[Groups([self::READ])]
     public ?int $id = null;
 
     #[ORM\ManyToOne(cascade: ['persist'], inversedBy: 'bikes')]
     #[ORM\JoinColumn(nullable: false)]
-    #[Groups([self::REPAIRER_READ, self::REPAIRER_WRITE])]
+    #[Groups([self::READ, self::WRITE])]
     public ?User $owner = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    #[Groups([self::REPAIRER_READ, self::REPAIRER_WRITE])]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $brand = null;
 
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: true)]
-    #[Groups([self::REPAIRER_READ, self::REPAIRER_WRITE])]
+    #[Groups([self::READ, self::WRITE])]
     public ?BikeType $bikeType = null;
 
     #[ORM\Column(length: 255, nullable: true)]
-    #[Groups([self::REPAIRER_READ, self::REPAIRER_WRITE])]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
-    #[Groups([self::REPAIRER_READ, self::REPAIRER_WRITE])]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $description = null;
 
     #[ORM\Column(type: Types::TEXT, nullable: true)]
-    #[Groups([self::REPAIRER_READ, self::REPAIRER_WRITE])]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $details = null;
 
     #[ORM\Column(nullable: true)]
@@ -59,19 +60,19 @@ class Bike
     #[ORM\ManyToOne(targetEntity: MediaObject::class)]
     #[ORM\JoinColumn(nullable: true)]
     #[ApiProperty(types: ['https://schema.org/image'])]
-    #[Groups([self::REPAIRER_READ])]
+    #[Groups([self::READ, self::WRITE])]
     public ?MediaObject $picture = null;
 
     #[ORM\ManyToOne(targetEntity: MediaObject::class)]
     #[ORM\JoinColumn(nullable: true)]
     #[ApiProperty(types: ['https://schema.org/image'])]
-    #[Groups([self::REPAIRER_READ])]
+    #[Groups([self::READ, self::WRITE])]
     public ?MediaObject $wheelPicture = null;
 
     #[ORM\ManyToOne(targetEntity: MediaObject::class)]
     #[ORM\JoinColumn(nullable: true)]
     #[ApiProperty(types: ['https://schema.org/image'])]
-    #[Groups([self::REPAIRER_READ])]
+    #[Groups([self::READ, self::WRITE])]
     public ?MediaObject $transmissionPicture = null;
 
     public function __construct()
