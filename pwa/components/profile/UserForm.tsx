94dc8cef4fa24171cfd498c9af8171b935fcb494
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-04-25 15:36:38 +0200

    improve homepage

--- pwa/components/profile/UserForm.tsx
+++ pwa/components/profile/UserForm.tsx
@@ -1,117 +1,136 @@
-import React, {ChangeEvent, useContext, useEffect, useState} from "react";
-import {validateEmail} from "@utils/emailValidator";
-import {validatePassword} from "@utils/passwordValidator";
-import TextField from "@mui/material/TextField";
+import React, {ChangeEvent, useContext, useEffect, useState} from 'react';
+import {validateEmail} from '@utils/emailValidator';
+import {validatePassword} from '@utils/passwordValidator';
+import TextField from '@mui/material/TextField';
 import {useRouter} from 'next/router';
-import {UserFormContext} from "@contexts/UserFormContext";
-import {User} from "@interfaces/User";
-import Box from "@mui/material/Box";
+import {UserFormContext} from '@contexts/UserFormContext';
+import {User} from '@interfaces/User';
+import Box from '@mui/material/Box';
 
 interface UserFormProps {
-    user: User|null;
+  user: User | null;
 }
 
 export const UserForm = ({user}: UserFormProps): JSX.Element => {
+  const router = useRouter();
+  const {
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    email,
+    setEmail,
+    password,
+    passwordError,
+    setPasswordError,
+    setPassword,
+    emailError,
+    setEmailError,
+    emailHelperText,
+    setEmailHelperText,
+    passwordInfo,
+    setPasswordInfo,
+  } = useContext(UserFormContext);
 
-    const router = useRouter();
-    const {firstName, setFirstName, lastName, setLastName, email, setEmail, password, passwordError,
-        setPasswordError, setPassword, emailError, setEmailError, emailHelperText, setEmailHelperText, passwordInfo, setPasswordInfo} = useContext(UserFormContext);
+  useEffect(() => {
+    if (user) {
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+    }
+  }, [user, setFirstName, setLastName]);
 
-    useEffect(() => {
-        if (user) {
-            setFirstName(user.firstName);
-            setLastName(user.lastName);
-        }
-    }, [user, setFirstName, setLastName]);
+  const handleChangeFirstName = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setFirstName(event.target.value);
+  };
 
-    const handleChangeFirstName = (event: ChangeEvent<HTMLInputElement>): void => {
-        setFirstName(event.target.value);
-    };
+  const handleChangeLastName = (event: ChangeEvent<HTMLInputElement>): void => {
+    setLastName(event.target.value);
+  };
 
-    const handleChangeLastName = (event: ChangeEvent<HTMLInputElement>): void => {
-        setLastName(event.target.value);
-    };
+  const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.target.value);
+    if (!validateEmail(event.target.value)) {
+      setEmailError(true);
+      setEmailHelperText('Veuillez entrer une adresse email valide.');
+    } else {
+      setEmailError(false);
+      setEmailHelperText('');
+    }
+  };
 
-    const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>): void => {
-        setEmail(event.target.value);
-        if (!validateEmail(event.target.value)) {
-            setEmailError(true);
-            setEmailHelperText('Veuillez entrer une adresse email valide.');
-        } else {
-            setEmailError(false);
-            setEmailHelperText('');
-        }
-    };
+  const handleChangePassword = (event: ChangeEvent<HTMLInputElement>): void => {
+    setPassword(event.target.value);
+    if (!validatePassword(event.target.value)) {
+      setPasswordError(true);
+      setPasswordInfo(
+        'Votre mot de passe doit contenir 12 caractères, une majuscule, un caractère spécial et des chiffres.'
+      );
+    } else {
+      setPasswordError(false);
+      setPasswordInfo('');
+    }
+  };
 
-    const handleChangePassword = (event: ChangeEvent<HTMLInputElement>): void => {
-        setPassword(event.target.value);
-        if (!validatePassword(event.target.value)) {
-            setPasswordError(true);
-            setPasswordInfo('Votre mot de passe doit contenir 12 caractères, une majuscule, un caractère spécial et des chiffres.');
-        } else {
-            setPasswordError(false);
-            setPasswordInfo('');
-        }
-    };
-
-    return (
-            <Box sx={{width: '50%'}}>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="Prénom"
-                    name="firstName"
-                    autoComplete="firstName"
-                    autoFocus
-                    value={firstName}
-                    onChange={handleChangeFirstName}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Nom de famille"
-                    name="lastName"
-                    autoComplete="lastName"
-                    autoFocus
-                    value={lastName}
-                    onChange={handleChangeLastName}
-                />
-                {
-                    !user &&  <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        error={emailError}
-                        helperText={emailHelperText}
-                        id="email"
-                        label="Email"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                        value={email}
-                        onChange={handleChangeEmail}
-                    />
-                }
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    error={passwordError}
-                    helperText={passwordInfo}
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                    value={password}
-                    onChange={handleChangePassword}
-                />
-            </Box>
-    );
+  return (
+    <Box>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="firstName"
+        label="Prénom"
+        name="firstName"
+        autoComplete="firstName"
+        autoFocus
+        value={firstName}
+        onChange={handleChangeFirstName}
+      />
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="lastName"
+        label="Nom de famille"
+        name="lastName"
+        autoComplete="lastName"
+        autoFocus
+        value={lastName}
+        onChange={handleChangeLastName}
+      />
+      {!user && (
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          error={emailError}
+          helperText={emailHelperText}
+          id="email"
+          label="Email"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={email}
+          onChange={handleChangeEmail}
+        />
+      )}
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        error={passwordError}
+        helperText={passwordInfo}
+        name="password"
+        label="Mot de passe"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        value={password}
+        onChange={handleChangePassword}
+      />
+    </Box>
+  );
 };
 
 export default UserForm;
--- pwa/pages/login.tsx
+++ pwa/pages/login.tsx
@@ -1,6 +1,6 @@
 import {NextPageWithLayout} from 'pages/_app';
 import React, {useState, ChangeEvent} from 'react';
-import Head from "next/head";
+import Head from 'next/head';
 import Avatar from '@mui/material/Avatar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Link from '@mui/material/Link';
@@ -8,134 +8,136 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Container from '@mui/material/Container';
-import Typography from "@mui/material/Typography";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import {useAuth} from 'contexts/AuthContext';
 import {useRouter} from 'next/router';
-import {CircularProgress} from "@mui/material";
-import WebsiteLayout from "@components/layout/WebsiteLayout";
+import {CircularProgress} from '@mui/material';
+import WebsiteLayout from '@components/layout/WebsiteLayout';
 
 const Login: NextPageWithLayout = ({}) => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [pendingLogin, setPendingLogin] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const {login} = useAuth();
+  const router = useRouter();
 
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [pendingLogin, setPendingLogin] = useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-    const {login} = useAuth();
-    const router = useRouter();
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setErrorMessage(null);
+    setPendingLogin(true);
+    const user = await login({
+      email: email,
+      password: password,
+    });
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    if (!!user) {
+      const next = Array.isArray(router.query.next)
+        ? router.query.next.join('')
+        : router.query.next || '/';
+      router.push(next);
+    } else {
+      setErrorMessage('Ces identifiants de connexion ne sont pas valides');
+    }
 
-        event.preventDefault();
-        setErrorMessage(null);
-        setPendingLogin(true);
-        const user = await login({
-            'email': email,
-            'password': password
-        });
+    setPendingLogin(false);
+  };
 
-        if (!!user) {
-            const next = Array.isArray(router.query.next) ? router.query.next.join('') : router.query.next || '/'
-            router.push(next)
-        } else {
-            setErrorMessage(
-                "Ces identifiants de connexion ne sont pas valides"
-            );
-        }
+  const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
 
-        setPendingLogin(false);
-    };
+  const handleChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
-    const handleChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
-        setEmail(event.target.value)
-    };
-
-    const handleChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value)
-    };
-
-    return (
-        <>
-            <div style={{width: "100vw", overflowX: "hidden"}}>
-                <Head>
-                    <title>Se connecter</title>
-                </Head>
-                <WebsiteLayout/>
-                <div style={{width: "100vw", marginBottom: '100px'}}>
-                    <Container component="main" maxWidth="xs">
-                        <CssBaseline />
-                        <Box
-                            sx={{
-                                marginTop: 8,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                            }}
-                        >
-                            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                                <LockOutlinedIcon />
-                            </Avatar>
-                            <Typography component="h1" variant="h5">
-                                Se connecter
-                            </Typography>
-                            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Addresse email"
-                                    name="email"
-                                    autoComplete="email"
-                                    autoFocus
-                                    value={email}
-                                    onChange={handleChangeEmail}
-                                />
-                                <TextField
-                                    margin="normal"
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="current-password"
-                                    value={password}
-                                    onChange={handleChangePassword}
-                                />
-                                <Button
-                                    type="submit"
-                                    fullWidth
-                                    variant="outlined"
-                                    sx={{ mt: 3, mb: 2 }}
-                                >
-                                    {!pendingLogin ? 'Se connecter' : <CircularProgress size={20} />}
-                                </Button>
-                                {errorMessage && (
-                                    <Typography variant="body1" color="error">
-                                        {errorMessage}
-                                    </Typography>
-                                )}
-                                <Grid container>
-                                    <Grid item xs>
-                                        <Link href="#" variant="body2">
-                                            Mot de passe oublié ?
-                                        </Link>
-                                    </Grid>
-                                    <Grid item>
-                                        <Link href="#" variant="body2">
-                                            S’inscrire
-                                        </Link>
-                                    </Grid>
-                                </Grid>
-                            </Box>
-                        </Box>
-                    </Container>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <div style={{width: '100vw', overflowX: 'hidden'}}>
+      <Head>
+        <title>Se connecter</title>
+      </Head>
+      <WebsiteLayout />
+      <div style={{width: '100vw', marginBottom: '100px'}}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Avatar sx={{m: 1, bgcolor: 'secondary.main'}}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Se connecter
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{mt: 1}}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Addresse email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={handleChangeEmail}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={handleChangePassword}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                sx={{mt: 3, mb: 2}}>
+                {!pendingLogin ? (
+                  'Se connecter'
+                ) : (
+                  <CircularProgress size={20} />
+                )}
+              </Button>
+              {errorMessage && (
+                <Typography variant="body1" color="error">
+                  {errorMessage}
+                </Typography>
+              )}
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Mot de passe oublié ?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    S’inscrire
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
