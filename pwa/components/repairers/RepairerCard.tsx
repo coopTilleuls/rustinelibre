0771906f326c3e94208790fc12ba8f3834e9fc9d
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-04-26 12:00:29 +0200

    improve style of a repairer page

--- pwa/components/repairers/RepairerCard.tsx
+++ pwa/components/repairers/RepairerCard.tsx
@@ -1,51 +1,69 @@
-import React from "react";
-import {Repairer} from "@interfaces/Repairer";
+import React from 'react';
+import {Repairer} from '@interfaces/Repairer';
 import {formatDate} from 'helpers/dateHelper';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import Link from "next/link";
-import {apiImageUrl} from "@helpers/apiImagesHelper";
+import Link from 'next/link';
+import {apiImageUrl} from '@helpers/apiImagesHelper';
 
 interface RepairerProps {
-    repairer: Repairer;
-    isSelect: boolean;
+  repairer: Repairer;
+  isSelect: boolean;
 }
 
-export const RepairerCard = ({repairer, isSelect}: RepairerProps): JSX.Element => {
-
-    return (
-        <Link href={`/reparateur/${repairer.id}`} passHref>
-            <Card sx={{ display: 'flex', marginBottom: '10px', backgroundColor: isSelect ? 'lightgreen' : 'white' }}>
-                <CardMedia
-                    component="img"
-                    sx={{ width: 151 }}
-                    image={repairer.thumbnail ? apiImageUrl(repairer.thumbnail.contentUrl) : 'https://cdn.cleanrider.com/uploads/2021/04/prime-reparation-velo_140920-3.jpg'}
-                    alt="Photo du réparateur"
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                    <CardContent sx={{ flex: '1 0 auto' }}>
-                        <Typography component="div" variant="h5">
-                            {repairer.name}
-                        </Typography>
-                        <Typography variant="subtitle1" color="text.secondary" component="div">
-                            {repairer.street}  <br/>
-                            {repairer.postcode} {repairer.city}
-                        </Typography>
-                        <Typography variant="subtitle2" color="text.secondary" component="div">
-                            {
-                                repairer.firstSlotAvailable !== undefined ?
-                                    <p>
-                                        <strong>Prochaine disponibilité : </strong>
-                                        {formatDate(repairer.firstSlotAvailable)}
-                                    </p> : 'Pas de créneau indiqué'
-                            }
-                        </Typography>
-                    </CardContent>
-                </Box>
-            </Card>
-        </Link>
-    )
+export const RepairerCard = ({
+  repairer,
+  isSelect,
+}: RepairerProps): JSX.Element => {
+  return (
+    <Link href={`/reparateur/${repairer.id}`} passHref>
+      <Card
+        sx={{
+          display: 'flex',
+          marginBottom: '10px',
+          backgroundColor: isSelect ? 'lightgreen' : 'white',
+        }}>
+        <CardMedia
+          component="img"
+          sx={{width: 151}}
+          image={
+            repairer.thumbnail
+              ? apiImageUrl(repairer.thumbnail.contentUrl)
+              : 'https://cdn.cleanrider.com/uploads/2021/04/prime-reparation-velo_140920-3.jpg'
+          }
+          alt="Photo du réparateur"
+        />
+        <Box sx={{display: 'flex', flexDirection: 'column'}}>
+          <CardContent sx={{flex: '1 0 auto'}}>
+            <Typography component="div" variant="h5">
+              {repairer.name}
+            </Typography>
+            <Typography
+              variant="subtitle1"
+              color="text.secondary"
+              component="div">
+              {repairer.street} <br />
+              {repairer.postcode} {repairer.city}
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              color="text.secondary"
+              component="div">
+              {repairer.firstSlotAvailable !== undefined ? (
+                <p>
+                  <strong>Prochaine disponibilité : </strong>
+                  {formatDate(repairer.firstSlotAvailable)}
+                </p>
+              ) : (
+                'Pas de créneau indiqué'
+              )}
+            </Typography>
+          </CardContent>
+        </Box>
+      </Card>
+    </Link>
+  );
 };
---
+++ pwa/components/repairers/RepairerPresentation.tsx
@@ -0,0 +1,147 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import useMediaQuery from '@hooks/useMediaQuery';
+import {Container, Box, Typography, Paper, Stack, Button} from '@mui/material';
+import FmdGoodIcon from '@mui/icons-material/FmdGood';
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import LocationCity from '@mui/icons-material/LocationCity';
+import {apiImageUrl} from '@helpers/apiImagesHelper';
+import {formatDate} from 'helpers/dateHelper';
+import {Repairer} from '@interfaces/Repairer';
+
+interface RepairerPresentationProps {
+  repairer: Repairer;
+}
+
+const RepairerPresentation = ({repairer}: RepairerPresentationProps) => {
+  const isMobile = useMediaQuery('(max-width: 1024px)');
+
+  return (
+    <Container maxWidth="md" sx={{padding: {xs: 0}}}>
+      <Paper elevation={isMobile ? 0 : 4} sx={{p: 3}}>
+        <Link href={`/reparateur/chercher-un-reparateur`}>
+          <Button variant="outlined">Retour</Button>
+        </Link>
+        <Stack
+          spacing={5}
+          marginBottom={4}
+          sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <Box>
+            <Typography
+              component="h1"
+              align="center"
+              fontSize={{xs: 34, md: 50}}
+              fontWeight={600}
+              sx={{mt: 2}}>
+              {repairer.name}
+            </Typography>
+            <Typography
+              component="h2"
+              align="center"
+              fontSize={{xs: 16, md: 20}}
+              color="primary">
+              {repairer.repairerType?.name}
+            </Typography>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'start',
+            }}>
+            <Typography
+              paragraph
+              fontSize={{xs: 14, md: 18}}
+              color="text.secondary"
+              sx={{display: 'flex', alignItems: 'center'}}>
+              <FmdGoodIcon color="primary" sx={{mr: 1}} /> {repairer.street}
+            </Typography>
+            <Typography
+              paragraph
+              fontSize={{xs: 14, md: 18}}
+              color="text.secondary"
+              sx={{display: 'flex', alignItems: 'center'}}>
+              <LocationCity color="primary" sx={{mr: 1}} />
+              {repairer.postcode} {repairer.city}
+            </Typography>
+            <Typography
+              paragraph
+              fontSize={{xs: 14, md: 18}}
+              color="text.secondary"
+              sx={{display: 'flex', alignItems: 'center'}}>
+              <PhoneAndroidIcon color="primary" sx={{mr: 1}} />{' '}
+              {repairer.mobilePhone}
+            </Typography>
+          </Box>
+          {repairer.thumbnail && (
+            <>
+              <Button variant="contained">Je réserve</Button>
+              <Image
+                src={apiImageUrl(repairer.thumbnail.contentUrl)}
+                alt={'Photo de profil du réparateur'}
+              />
+            </>
+          )}
+          <Box width={{md: '70%'}}>
+            {repairer.description && (
+              <>
+                <Typography fontSize={{xs: 16, md: 18}} fontWeight={600}>
+                  Description :
+                </Typography>
+                <Typography
+                  paragraph
+                  fontSize={{xs: 16, md: 18}}
+                  color="text.secondary">
+                  {repairer.description}
+                </Typography>
+              </>
+            )}
+            {repairer.firstSlotAvailable && (
+              <>
+                <Typography fontSize={{xs: 16, md: 18}} fontWeight={600}>
+                  Prochain rendez-vous disponible :
+                </Typography>
+                <Typography
+                  paragraph
+                  fontSize={{xs: 16, md: 18}}
+                  color="text.secondary">
+                  {formatDate(repairer.firstSlotAvailable)}
+                </Typography>
+              </>
+            )}
+            {repairer.openingHours && (
+              <>
+                <Typography fontSize={{xs: 16, md: 18}} fontWeight={600}>
+                  Horaires :
+                </Typography>
+                <Typography
+                  paragraph
+                  fontSize={{xs: 16, md: 18}}
+                  color="text.secondary">
+                  {repairer.openingHours}
+                </Typography>
+              </>
+            )}
+            {repairer.optionalPage && (
+              <>
+                <Typography fontSize={{xs: 16, md: 18}} fontWeight={600}>
+                  Informations complémentaires :
+                </Typography>
+                <Typography
+                  paragraph
+                  fontSize={{xs: 16, md: 18}}
+                  color="text.secondary">
+                  {repairer.optionalPage}
+                </Typography>
+              </>
+            )}
+          </Box>
+          <Button variant="contained">Je réserve</Button>
+        </Stack>
+      </Paper>
+    </Container>
+  );
+};
+
+export default RepairerPresentation;
