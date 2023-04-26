e69518ea9c968134da76f1f67e5106229008b71b
clementtalleu <clement.talleu@gmail.com>
2023-04-26 11:39:13 +0200

    fix typo

--- api/migrations/Version20230426083518.php
+++ api/migrations/Version20230426083518.php
@@ -7,7 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-gfinal class Version20230426083518 extends AbstractMigration
+final class Version20230426083518 extends AbstractMigration
 {
     public function getDescription(): string
     {
