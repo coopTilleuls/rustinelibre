de1b6e9fec8a693f5052657a236484972dcbfd07
Davy <davy.beauzil72@gmail.com>
2023-04-26 11:41:32 +0200

    refactor: improve bike tests + few others tests

--- api/tests/Bike/Security/CreateTest.php
+++ api/tests/Bike/Security/CreateTest.php
@@ -4,26 +4,39 @@ declare(strict_types=1);
 
 namespace App\Tests\Bike\Security;
 
-use App\Tests\Bike\BikeAbstractTestCase;
+use App\Repository\BikeTypeRepository;
+use App\Repository\MediaObjectRepository;
+use App\Tests\AbstractTestCase;
+use App\Tests\Trait\BikeTypeTrait;
+use App\Tests\Trait\MediaTrait;
 
-class CreateTest extends BikeAbstractTestCase
+class CreateTest extends AbstractTestCase
 {
+    use MediaTrait;
+    use BikeTypeTrait;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->mediaRepository = self::getContainer()->get(MediaObjectRepository::class);
+        $this->bikeTypeRepository = self::getContainer()->get(BikeTypeRepository::class);
+    }
+
     public function testCreateBikeWithUser(): void
     {
-        $pictureId = $this->addMedia();
-        $wheelPictureId = $this->addMedia();
-        $transmissionPictureId = $this->addMedia();
+        $media = $this->getMedia();
+        $bikeType = $this->getBikeType();
 
         $this->createClientAuthAsUser()->request('POST', '/bikes', [
             'headers' => ['Content-Type' => 'application/json'],
             'json' => [
                 'brand' => 'Test',
-                'bikeType' => sprintf('/bike_types/%d', $this->bikeTypes[0]->id),
+                'bikeType' => sprintf('/bike_types/%d', $bikeType->id),
                 'name' => 'Test',
                 'description' => 'Test',
-                'picture' => sprintf('/media_objects/%s', $pictureId),
-                'wheelPicture' => sprintf('/media_objects/%s', $wheelPictureId),
-                'transmissionPicture' => sprintf('/media_objects/%s', $transmissionPictureId),
+                'picture' => sprintf('/media_objects/%s', $media->id),
+                'wheelPicture' => sprintf('/media_objects/%s', $media->id),
+                'transmissionPicture' => sprintf('/media_objects/%s', $media->id),
             ],
         ]);
         self::assertResponseStatusCodeSame(201);
@@ -31,20 +44,19 @@ class CreateTest extends BikeAbstractTestCase
 
     public function testCreateBikeWithUnauthenticated(): void
     {
-        $pictureId = $this->addMedia();
-        $wheelPictureId = $this->addMedia();
-        $transmissionPictureId = $this->addMedia();
+        $media = $this->getMedia();
+        $bikeType = $this->getBikeType();
 
         self::createClient()->request('POST', '/bikes', [
             'headers' => ['Content-Type' => 'application/json'],
             'json' => [
                 'brand' => 'Test',
-                'bikeType' => sprintf('/bike_types/%d', $this->bikeTypes[0]->id),
+                'bikeType' => sprintf('/bike_types/%d', $bikeType->id),
                 'name' => 'Test',
                 'description' => 'Test',
-                'picture' => sprintf('/media_objects/%s', $pictureId),
-                'wheelPicture' => sprintf('/media_objects/%s', $wheelPictureId),
-                'transmissionPicture' => sprintf('/media_objects/%s', $transmissionPictureId),
+                'picture' => sprintf('/media_objects/%s', $media->id),
+                'wheelPicture' => sprintf('/media_objects/%s', $media->id),
+                'transmissionPicture' => sprintf('/media_objects/%s', $media->id),
             ],
         ]);
         self::assertResponseStatusCodeSame(401);
--- api/tests/Bike/Security/UpdateTest.php
+++ api/tests/Bike/Security/UpdateTest.php
@@ -4,27 +4,44 @@ declare(strict_types=1);
 
 namespace App\Tests\Bike\Security;
 
-use App\Tests\Bike\BikeAbstractTestCase;
+use App\Repository\BikeRepository;
+use App\Repository\BikeTypeRepository;
+use App\Repository\MediaObjectRepository;
+use App\Tests\AbstractTestCase;
+use App\Tests\Trait\BikeTrait;
+use App\Tests\Trait\BikeTypeTrait;
+use App\Tests\Trait\MediaTrait;
 
-class UpdateTest extends BikeAbstractTestCase
+class UpdateTest extends AbstractTestCase
 {
+    use BikeTrait;
+    use MediaTrait;
+    use BikeTypeTrait;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->bikeTypeRepository = self::getContainer()->get(BikeTypeRepository::class);
+        $this->bikeRepository = self::getContainer()->get(BikeRepository::class);
+        $this->mediaRepository = self::getContainer()->get(MediaObjectRepository::class);
+    }
+
     public function testUserCanUpdateHisBike(): void
     {
-        $pictureId = $this->addMedia();
-        $wheelPictureId = $this->addMedia();
-        $transmissionPictureId = $this->addMedia();
-        $bike = $this->bikes[0];
+        $media = $this->getMedia();
+        $bike = $this->getBike();
+        $bikeType = $this->getBikeType();
 
         $this->createClientWithUser($bike->owner)->request('PUT', sprintf('/bikes/%d', $bike->id), [
             'headers' => ['Content-Type' => 'application/json'],
             'json' => [
                 'brand' => 'Test',
-                'bikeType' => sprintf('/bike_types/%d', $this->bikeTypes[0]->id),
+                'bikeType' => sprintf('/bike_types/%d', $bikeType->id),
                 'name' => 'Test',
                 'description' => 'Test',
-                'picture' => sprintf('/media_objects/%s', $pictureId),
-                'wheelPicture' => sprintf('/media_objects/%s', $wheelPictureId),
-                'transmissionPicture' => sprintf('/media_objects/%s', $transmissionPictureId),
+                'picture' => sprintf('/media_objects/%s', $media->id),
+                'wheelPicture' => sprintf('/media_objects/%s', $media->id),
+                'transmissionPicture' => sprintf('/media_objects/%s', $media->id),
             ],
         ]);
         self::assertResponseStatusCodeSame(200);
@@ -32,21 +49,20 @@ class UpdateTest extends BikeAbstractTestCase
 
     public function testAdminCanUpdateBike(): void
     {
-        $pictureId = $this->addMedia();
-        $wheelPictureId = $this->addMedia();
-        $transmissionPictureId = $this->addMedia();
-        $bike = $this->bikes[0];
+        $media = $this->getMedia();
+        $bike = $this->getBike();
+        $bikeType = $this->getBikeType();
 
         $this->createClientAuthAsAdmin()->request('PUT', sprintf('/bikes/%d', $bike->id), [
             'headers' => ['Content-Type' => 'application/json'],
             'json' => [
                 'brand' => 'Test',
-                'bikeType' => sprintf('/bike_types/%d', $this->bikeTypes[0]->id),
+                'bikeType' => sprintf('/bike_types/%d', $bikeType->id),
                 'name' => 'Test',
                 'description' => 'Test',
-                'picture' => sprintf('/media_objects/%s', $pictureId),
-                'wheelPicture' => sprintf('/media_objects/%s', $wheelPictureId),
-                'transmissionPicture' => sprintf('/media_objects/%s', $transmissionPictureId),
+                'picture' => sprintf('/media_objects/%s', $media->id),
+                'wheelPicture' => sprintf('/media_objects/%s', $media->id),
+                'transmissionPicture' => sprintf('/media_objects/%s', $media->id),
             ],
         ]);
         self::assertResponseStatusCodeSame(200);
@@ -54,21 +70,20 @@ class UpdateTest extends BikeAbstractTestCase
 
     public function testUserCannotUpdateOtherBike(): void
     {
-        $pictureId = $this->addMedia();
-        $wheelPictureId = $this->addMedia();
-        $transmissionPictureId = $this->addMedia();
-        $bike = $this->bikes[0];
+        $media = $this->getMedia();
+        $bike = $this->getBike();
+        $bikeType = $this->getBikeType();
 
         $this->createClientAuthAsUser()->request('PUT', sprintf('/bikes/%d', $bike->id), [
             'headers' => ['Content-Type' => 'application/json'],
             'json' => [
                 'brand' => 'Test',
-                'bikeType' => sprintf('/bike_types/%d', $this->bikeTypes[0]->id),
+                'bikeType' => sprintf('/bike_types/%d', $bikeType->id),
                 'name' => 'Test',
                 'description' => 'Test',
-                'picture' => sprintf('/media_objects/%s', $pictureId),
-                'wheelPicture' => sprintf('/media_objects/%s', $wheelPictureId),
-                'transmissionPicture' => sprintf('/media_objects/%s', $transmissionPictureId),
+                'picture' => sprintf('/media_objects/%s', $media->id),
+                'wheelPicture' => sprintf('/media_objects/%s', $media->id),
+                'transmissionPicture' => sprintf('/media_objects/%s', $media->id),
             ],
         ]);
         self::assertResponseStatusCodeSame(403);
