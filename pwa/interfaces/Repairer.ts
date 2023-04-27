9a4a863b177a0c13b45460a7f3e542522b552c18
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-04-27 13:38:07 +0200

    adding distance pin on repairer card and showmap button for mobile version (#265)

--- pwa/interfaces/Repairer.ts
+++ pwa/interfaces/Repairer.ts
@@ -1,30 +1,31 @@
 import {User} from '@interfaces/User';
 import {BikeType} from '@interfaces/BikeType';
-import {RepairerType} from "@interfaces/RepairerType";
-import {MediaObject} from "@interfaces/MediaObject";
+import {RepairerType} from '@interfaces/RepairerType';
+import {MediaObject} from '@interfaces/MediaObject';
 
 export interface Repairer {
-    '@id': string;
-    '@type': string;
-    id: string;
-    owner: User;
-    name?: string;
-    description?: string;
-    city?: string;
-    postcode?: string;
-    country?: string;
-    mobilePhone?: string;
-    thumbnail?: MediaObject;
-    descriptionPicture?: MediaObject;
-    street?: string;
-    rrule: string;
-    latitude?: string;
-    longitude?: string;
-    firstSlotAvailable?: string,
-    comment?: string,
-    bikeTypesSupported: BikeType[],
-    repairerType: RepairerType,
-    enabled: boolean,
-    openingHours?: string
-    optionalPage?: string
+  '@id': string;
+  '@type': string;
+  id: string;
+  owner: User;
+  name?: string;
+  description?: string;
+  city?: string;
+  distance?: number;
+  postcode?: string;
+  country?: string;
+  mobilePhone?: string;
+  thumbnail?: MediaObject;
+  descriptionPicture?: MediaObject;
+  street?: string;
+  rrule: string;
+  latitude?: string;
+  longitude?: string;
+  firstSlotAvailable?: string;
+  comment?: string;
+  bikeTypesSupported: BikeType[];
+  repairerType: RepairerType;
+  enabled: boolean;
+  openingHours?: string;
+  optionalPage?: string;
 }
