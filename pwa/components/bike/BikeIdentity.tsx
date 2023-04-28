03ebff47ffdec23ad53141d7193c331eea063306
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-04-28 10:45:23 +0200

    improve all pages about bikes

--- pwa/components/bike/BikeIdentity.tsx
+++ pwa/components/bike/BikeIdentity.tsx
@@ -1,161 +1,211 @@
-import React, {ChangeEvent, useState} from "react";
-import {Bike} from "@interfaces/Bike";
-import Box from "@mui/material/Box";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import {BikeType} from "@interfaces/BikeType";
-import Select, {SelectChangeEvent} from "@mui/material/Select";
-import {RequestBody} from "@interfaces/Resource";
-import {bikeResource} from "@resources/bikeResource";
-import AddIcon from "@mui/icons-material/Add";
-import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
+import React, {ChangeEvent, useState} from 'react';
+import {bikeResource} from '@resources/bikeResource';
+import {
+  Box,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Alert,
+  CircularProgress,
+  MenuItem,
+  TextField,
+} from '@mui/material';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import AddIcon from '@mui/icons-material/Add';
 import SaveIcon from '@mui/icons-material/Save';
-import {Alert, CircularProgress} from "@mui/material";
-import BikeIdentityPhoto from "@components/bike/BikeIdentityPhoto";
+import BikeIdentityPhoto from '@components/bike/BikeIdentityPhoto';
+import {Bike} from '@interfaces/Bike';
+import {RequestBody} from '@interfaces/Resource';
+import {BikeType} from '@interfaces/BikeType';
 
 type BikeIdentityProps = {
-    bike: Bike;
-    bikeTypes: BikeType[];
+  bike: Bike;
+  bikeTypes: BikeType[];
 };
 
 const BikeIdentity = ({bike, bikeTypes}: BikeIdentityProps): JSX.Element => {
+  const [addDescription, setAddDescription] = useState<boolean>(false);
+  const [brand, setBrand] = useState<string>(bike.brand ? bike.brand : '');
+  const [description, setDescription] = useState<string>(
+    bike.description ? bike.description : ''
+  );
+  const [pendingUpdate, setPendingUpdate] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(bike.name ? bike.name : '');
+  const [selectedBike, setSelectedBike] = useState<BikeType | null>(
+    bike.bikeType ? bike.bikeType : null
+  );
+  const [updateSuccess, setUpdateSuccess] = useState<boolean>(false);
 
-    const [addDescription, setAddDescription] = useState<boolean>(false);
-    const [brand, setBrand] = useState<string>(bike.brand ? bike.brand : '');
-    const [description, setDescription] = useState<string>(bike.description ? bike.description : '');
-    const [pendingUpdate, setPendingUpdate] = useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-    const [name, setName] = useState<string | null>(bike.name ? bike.name : '');
-    const [selectedBike, setSelectedBike] = useState<BikeType|null>(bike.bikeType ? bike.bikeType : null);
-    const [updateSuccess, setUpdateSuccess] = useState<boolean>(false);
+  const handleBikeChange = (event: SelectChangeEvent): void => {
+    const selectedBikeType = bikeTypes.find(
+      (bt) => bt.name === event.target.value
+    );
+    setSelectedBike(selectedBikeType ? selectedBikeType : null);
+  };
 
-    const handleBikeChange = (event: SelectChangeEvent): void => {
-        const selectedBikeType = bikeTypes.find((bt) => bt.name === event.target.value);
-        setSelectedBike(selectedBikeType ? selectedBikeType : null);
-    };
+  const handleChangeName = (event: ChangeEvent<HTMLInputElement>): void => {
+    setName(event.target.value);
+  };
 
-    const handleChangeName = (event: ChangeEvent<HTMLInputElement>): void => {
-        setName(event.target.value);
-    };
+  const handleChangeBrand = (event: ChangeEvent<HTMLInputElement>): void => {
+    setBrand(event.target.value);
+  };
 
-    const handleChangeBrand = (event: ChangeEvent<HTMLInputElement>): void => {
-        setBrand(event.target.value);
-    };
+  const handleChangeDescription = (
+    event: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setDescription(event.target.value);
+  };
 
-    const handleChangeDescription = (event: ChangeEvent<HTMLInputElement>): void => {
-        setDescription(event.target.value);
-    };
+  const handleAddDescription = (): void => {
+    setAddDescription(true);
+  };
 
-    const handleAddDescription = (): void => {
-        setAddDescription(true);
-    };
+  const handleSubmit = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): Promise<void> => {
+    event.preventDefault();
+    if (!name || !selectedBike) {
+      return;
+    }
 
-    const handleSubmit = async (event:  React.MouseEvent<HTMLButtonElement, MouseEvent>): Promise<void> => {
+    setErrorMessage(null);
+    setPendingUpdate(true);
 
-        event.preventDefault();
-        if (!name || !selectedBike) {
-            return;
-        }
+    try {
+      let bodyRequest: RequestBody = {
+        name: name,
+        selectedBike: selectedBike['@id'],
+      };
+      if (brand) {
+        bodyRequest['brand'] = brand;
+      }
+      if (description) {
+        bodyRequest['description'] = description;
+      }
 
-        setErrorMessage(null);
-        setPendingUpdate(true);
+      bike = await bikeResource.put(bike['@id'], bodyRequest);
+    } catch (e) {
+      setErrorMessage('Mise à jour du vélo impossible');
+    }
 
-        try {
-            let bodyRequest: RequestBody = {
-                'name': name,
-                'selectedBike': selectedBike['@id'],
-            };
-            if (brand) {
-                bodyRequest['brand'] = brand;
-            }
-            if (description) {
-                bodyRequest['description'] = description;
-            }
+    if (bike) {
+      setUpdateSuccess(true);
+      setTimeout(() => {
+        setUpdateSuccess(false);
+      }, 3000);
+    }
+    setPendingUpdate(false);
+  };
 
-            bike = await bikeResource.put(bike['@id'], bodyRequest);
-        } catch (e) {
-            setErrorMessage('Mise à jour du vélo impossible');
-        }
+  return (
+    <Box width={'100%'}>
+      <Card>
+        <CardContent>
+          {updateSuccess && <Alert severity="success">Vélo mis à jour</Alert>}
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="name"
+            label="Nom"
+            name="name"
+            autoComplete="name"
+            autoFocus
+            value={name}
+            onChange={handleChangeName}
+          />
+          <Select
+            required
+            onChange={handleBikeChange}
+            value={selectedBike?.name}
+            style={{width: '100%'}}>
+            <MenuItem disabled value="">
+              Choisissez un type de vélo
+            </MenuItem>
+            {bikeTypes.map((bike) => (
+              <MenuItem key={bike.id} value={bike.name}>
+                {bike.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="brand"
+            label="Marque"
+            name="name"
+            autoComplete="brand"
+            autoFocus
+            value={brand}
+            onChange={handleChangeBrand}
+          />
+          {addDescription && (
+            <TextField
+              margin="normal"
+              placeholder="Description de votre vélo"
+              multiline
+              fullWidth
+              value={description}
+              rows={3}
+              maxRows={6}
+              onChange={handleChangeDescription}
+            />
+          )}
+        </CardContent>
+        <CardActions
+          sx={{
+            display: 'flex',
+            flexDirection: {xs: 'column', md: 'row'},
+            justifyContent: {md: 'center'},
+          }}>
+          {!addDescription && (
+            <Button
+              onClick={handleAddDescription}
+              size="small"
+              variant="contained">
+              <AddIcon /> Ajouter une description
+            </Button>
+          )}
+          {pendingUpdate ? (
+            <CircularProgress />
+          ) : (
+            <Button
+              onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+                handleSubmit(e)
+              }
+              size="small"
+              variant="contained"
+              sx={{my: 2}}>
+              <SaveIcon /> Enregistrer
+            </Button>
+          )}
+        </CardActions>
+      </Card>
 
-        if (bike) {
-            setUpdateSuccess(true);
-            setTimeout(() => {setUpdateSuccess(false);}, 3000);
-        }
-        setPendingUpdate(false);
-    };
-
-
-    return (
-        <Box
-            sx={{
-                marginTop: 4,
-            }}
-        >
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    {updateSuccess && <Alert severity="success">Vélo mis à jour</Alert>}
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="name"
-                        label="Nom"
-                        name="name"
-                        autoComplete="name"
-                        autoFocus
-                        value={name}
-                        onChange={handleChangeName}
-                    />
-                    <Select
-                        required
-                        onChange={handleBikeChange}
-                        value={selectedBike?.name}
-                        style={{width: '100%'}}
-                    >
-                        <MenuItem disabled value="">Choisissez un type de vélo</MenuItem>
-                        {bikeTypes.map((bike) => (
-                            <MenuItem key={bike.id} value={bike.name}>{bike.name}</MenuItem>
-                        ))}
-                    </Select>
-                    <TextField
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="brand"
-                        label="Marque"
-                        name="name"
-                        autoComplete="brand"
-                        autoFocus
-                        value={brand}
-                        onChange={handleChangeBrand}
-                    />
-                    {
-                        addDescription && <TextField
-                            margin="normal"
-                            placeholder="Description de votre vélo"
-                            multiline
-                            fullWidth
-                            value={description}
-                            rows={3}
-                            maxRows={6}
-                            onChange={handleChangeDescription}
-                        />
-                    }
-                </CardContent>
-                <CardActions>
-                    {!addDescription && <Button onClick={handleAddDescription} size="small"><AddIcon /> Ajouter un détail</Button>}
-                    <Button onClick={(e :  React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleSubmit(e)} size="small"><SaveIcon /> {pendingUpdate ? <CircularProgress /> : 'Enregistrer'}</Button>
-                </CardActions>
-            </Card>
-
-            <BikeIdentityPhoto bike={bike} photo={bike.picture ? bike.picture : null} propertyName='picture' title="Photo du vélo" />
-            <BikeIdentityPhoto bike={bike} photo={bike.wheelPicture ? bike.wheelPicture : null} propertyName='wheelPicture' title="Photo roue" />
-            <BikeIdentityPhoto bike={bike} photo={bike.transmissionPicture ? bike.transmissionPicture : null} propertyName='transmissionPicture' title="Photo transmission" />
-        </Box>
-    )
-}
+      <BikeIdentityPhoto
+        bike={bike}
+        photo={bike.picture ? bike.picture : null}
+        propertyName="picture"
+        title="Photo du vélo"
+      />
+      <BikeIdentityPhoto
+        bike={bike}
+        photo={bike.wheelPicture ? bike.wheelPicture : null}
+        propertyName="wheelPicture"
+        title="Photo roue"
+      />
+      <BikeIdentityPhoto
+        bike={bike}
+        photo={bike.transmissionPicture ? bike.transmissionPicture : null}
+        propertyName="transmissionPicture"
+        title="Photo transmission"
+      />
+    </Box>
+  );
+};
 
 export default BikeIdentity;
--- pwa/components/bike/BikeTabs.tsx
+++ pwa/components/bike/BikeTabs.tsx
@@ -1,66 +1,72 @@
-import React, {useEffect, useState} from "react";
-import {Bike} from "@interfaces/Bike";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
-import BikeIdentity from "@components/bike/BikeIdentity";
-import {BikeType} from "@interfaces/BikeType";
-import BikeMaintenance from "@components/bike/BikeMaintenance";
-import {Collection} from "@interfaces/Resource";
-import {Maintenance} from "@interfaces/Maintenance";
-import {maintenanceResource} from "@resources/MaintenanceResource";
-import {useAccount} from "@contexts/AuthContext";
+import React, {useEffect, useState} from 'react';
+import {Bike} from '@interfaces/Bike';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import BikeIdentity from '@components/bike/BikeIdentity';
+import {BikeType} from '@interfaces/BikeType';
+import BikeMaintenance from '@components/bike/BikeMaintenance';
+import {Collection} from '@interfaces/Resource';
+import {Maintenance} from '@interfaces/Maintenance';
+import {maintenanceResource} from '@resources/MaintenanceResource';
+import {useAccount} from '@contexts/AuthContext';
 
 type BikeTabsProps = {
-    bike: Bike;
-    bikeTypes: BikeType[];
+  bike: Bike;
+  bikeTypes: BikeType[];
 };
 
 const BikeTabs = ({bike, bikeTypes}: BikeTabsProps): JSX.Element => {
+  const user = useAccount({});
+  const [tabValue, setTabValue] = useState<number>(0);
+  const [maintenances, setMaintenances] = useState<Maintenance[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const user = useAccount({});
-    const [tabValue, setTabValue] = useState<number>(0);
-    const [maintenances, setMaintenances] = useState<Maintenance[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
-    const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
-        setTabValue(newValue);
-    };
-
-    async function fetchMaintenance() {
-        if (user) {
-            setLoading(true);
-            const maintenanceCollection: Collection<Maintenance> = await maintenanceResource.getAll(true, {bike: bike.id, 'order[repairDate]': 'DESC'});
-            setMaintenances(maintenanceCollection['hydra:member']);
-            setLoading(false);
-        }
+  async function fetchMaintenance() {
+    if (user) {
+      setLoading(true);
+      const maintenanceCollection: Collection<Maintenance> =
+        await maintenanceResource.getAll(true, {
+          bike: bike.id,
+          'order[repairDate]': 'DESC',
+        });
+      setMaintenances(maintenanceCollection['hydra:member']);
+      setLoading(false);
     }
+  }
 
-    useEffect(() => {
-        fetchMaintenance();
-    }, [user]); // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    fetchMaintenance();
+  }, [user]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    return (
-        <Box>
-            <Tabs
-                value={tabValue}
-                onChange={handleChangeTab}
-                aria-label='bike tabs'
-            >
-                <Tab label="Fiche d'identité" />
-                <Tab label='Réparations' />
-            </Tabs>
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Tabs
+        value={tabValue}
+        onChange={handleChangeTab}
+        aria-label="bike tabs"
+        sx={{width: '100%'}}>
+        <Tab label="Fiche d'identité" sx={{width: '50%'}} />
+        <Tab label="Réparations" sx={{width: '50%'}} />
+      </Tabs>
 
-            <Box sx={{ marginTop: 2 }}>
-                {tabValue === 0 && (
-                    <BikeIdentity bike={bike} bikeTypes={bikeTypes} />
-                )}
-                {tabValue === 1 && (
-                    <BikeMaintenance bike={bike} maintenances={maintenances} loading={loading} fetchMaintenance={fetchMaintenance} />
-                )}
-            </Box>
-        </Box>
-    )
-}
+      <Box sx={{marginTop: 2}}>
+        {tabValue === 0 && <BikeIdentity bike={bike} bikeTypes={bikeTypes} />}
+        {tabValue === 1 && (
+          <BikeMaintenance
+            bike={bike}
+            maintenances={maintenances}
+            loading={loading}
+            fetchMaintenance={fetchMaintenance}
+          />
+        )}
+      </Box>
+    </Box>
+  );
+};
 
 export default BikeTabs;
