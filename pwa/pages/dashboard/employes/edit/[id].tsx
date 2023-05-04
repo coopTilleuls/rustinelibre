7727035399020924287311e6926de140ff0bcbf3
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-05-04 15:41:28 +0200

    clean inputs in employee creation case (#310)

--- pwa/pages/dashboard/employes/edit/[id].tsx
+++ pwa/pages/dashboard/employes/edit/[id].tsx
@@ -1,46 +1,53 @@
 import {NextPageWithLayout} from 'pages/_app';
 import React, {useState, useEffect, useContext} from 'react';
-import Head from "next/head";
-import DashboardLayout from "@components/dashboard/DashboardLayout";
-import Box from "@mui/material/Box";
-import {useRouter} from "next/router";
-import {RepairerEmployee} from "@interfaces/RepairerEmployee";
-import {repairerEmployeesResource} from "@resources/repairerEmployeesResource";
-import {CircularProgress} from "@mui/material";
-import EmployeeForm from "@components/dashboard/employees/EmployeeForm";
+import Head from 'next/head';
+import DashboardLayout from '@components/dashboard/DashboardLayout';
+import Box from '@mui/material/Box';
+import {useRouter} from 'next/router';
+import {RepairerEmployee} from '@interfaces/RepairerEmployee';
+import {repairerEmployeesResource} from '@resources/repairerEmployeesResource';
+import {CircularProgress} from '@mui/material';
+import EmployeeForm from '@components/dashboard/employees/EmployeeForm';
 
 const EditEmployee: NextPageWithLayout = () => {
+  const router = useRouter();
+  const {id} = router.query;
+  const [repairerEmployee, setRepairerEmployee] =
+    useState<RepairerEmployee | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
-    const router = useRouter();
-    const { id } = router.query;
-    const [repairerEmployee, setRepairerEmployee] = useState<RepairerEmployee|null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    async function fetchEmployee() {
+      if (typeof id === 'string' && id.length > 0) {
+        setLoading(true);
+        const employeeFetch: RepairerEmployee =
+          await repairerEmployeesResource.getById(id);
+        setRepairerEmployee(employeeFetch);
+        setLoading(false);
+      }
+    }
+    if (id) {
+      fetchEmployee();
+    }
+  }, [id]);
 
-    useEffect(() => {
-        async function fetchEmployee() {
-            if (typeof id === 'string' && id.length > 0) {
-                setLoading(true);
-                const employeeFetch: RepairerEmployee = await repairerEmployeesResource.getById(id);
-                setRepairerEmployee(employeeFetch);
-                setLoading(false);
-            }
-        }
-        if (id) {
-            fetchEmployee();
-        }
-    }, [id]);
-
-    return (
-        <>
-            <Head>
-                <title>Éditer un réparateur</title>
-            </Head>
-            <DashboardLayout />
-            <Box component="main" sx={{ marginLeft: '20%', marginRight: '5%', marginTop: '100px' }}>
-                {loading ? <CircularProgress /> : <EmployeeForm repairerEmployee={repairerEmployee} edit={true} />}
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Éditer un réparateur</title>
+      </Head>
+      <DashboardLayout />
+      <Box
+        component="main"
+        sx={{marginLeft: '20%', marginRight: '5%', marginTop: '100px'}}>
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <EmployeeForm repairerEmployee={repairerEmployee} edit />
+        )}
+      </Box>
+    </>
+  );
 };
 
 export default EditEmployee;
