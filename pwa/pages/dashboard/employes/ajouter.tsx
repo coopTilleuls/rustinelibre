43a26ed246405c8010815289f8d8a74bc5ebce65
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-05-04 10:49:49 +0200

    clean inputs in employee creation case

--- pwa/pages/dashboard/employes/ajouter.tsx
+++ pwa/pages/dashboard/employes/ajouter.tsx
@@ -1,23 +1,24 @@
 import {NextPageWithLayout} from 'pages/_app';
 import React from 'react';
-import Head from "next/head";
-import DashboardLayout from "@components/dashboard/DashboardLayout";
-import Box from "@mui/material/Box";
-import EmployeeForm from "@components/dashboard/employees/EmployeeForm";
+import Head from 'next/head';
+import DashboardLayout from '@components/dashboard/DashboardLayout';
+import Box from '@mui/material/Box';
+import EmployeeForm from '@components/dashboard/employees/EmployeeForm';
 
 const AddEmployee: NextPageWithLayout = () => {
-
-    return (
-        <>
-            <Head>
-                <title>Ajouter un réparateur</title>
-            </Head>
-            <DashboardLayout />
-            <Box component="main" sx={{ marginLeft: '20%', marginRight: '5%', marginTop: '100px', }}>
-                <EmployeeForm repairerEmployee={null} edit={false} />
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Ajouter un réparateur</title>
+      </Head>
+      <DashboardLayout />
+      <Box
+        component="main"
+        sx={{marginLeft: '20%', marginRight: '5%', marginTop: '100px'}}>
+        <EmployeeForm />
+      </Box>
+    </>
+  );
 };
 
 export default AddEmployee;
