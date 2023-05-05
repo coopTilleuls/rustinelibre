999497bc9be18f461b50d408bafc24f4ded907af
clementtalleu <clement.talleu@gmail.com>
2023-05-05 15:15:31 +0200

    remove useless empty obj

--- pwa/utils/apiCity.ts
+++ pwa/utils/apiCity.ts
@@ -11,8 +11,7 @@ const nominatimCities = async (search: string) => {
 const gouvCities = async (search: string) => {
 
     search = encodeURIComponent(search);
-    const response = await fetch(`https://api-adresse.data.gouv.fr/search/?q=${search}&type=municipality`, {
-    });
+    const response = await fetch(`https://api-adresse.data.gouv.fr/search/?q=${search}&type=municipality`);
 
     return await response.json()
         .then((data) => {
