d54e555a933b677958056ee93c71b0e2427a05dd
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-05-11 08:15:16 +0200

    fix menu (#344)

---
+++ pwa/data/menu.ts
@@ -0,0 +1,40 @@
+export const menuNotLogged = [
+  {
+    name: 'Notre collectif',
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'Inscription',
+    link: '/inscription',
+  },
+  {
+    name: 'Liste des réparateurs',
+    link: '/reparateur/chercher-un-reparateur',
+  },
+  {
+    name: 'FAQ',
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'Devenir réparateur',
+    link: '/reparateur/inscription',
+  },
+];
+
+export const menuLogged = [
+  {
+    name: 'Notre collectif',
+    link: '/',
+    disabled: true,
+  },
+  {
+    name: 'Liste des réparateurs',
+    link: '/reparateur/chercher-un-reparateur',
+  },
+  {
+    name: 'FAQ',
+    link: '/',
+  },
+];
