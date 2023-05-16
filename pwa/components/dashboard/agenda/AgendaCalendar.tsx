ab6183625a3963405ff4622a2dbe17c0c76ab2df
clementtalleu <clement.talleu@gmail.com>
2023-05-16 12:05:11 +0200

    wip

--- pwa/components/dashboard/agenda/AgendaCalendar.tsx
+++ pwa/components/dashboard/agenda/AgendaCalendar.tsx
@@ -5,21 +5,33 @@ import {Typography} from "@mui/material";
 import Box from "@mui/material/Box";
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from '@fullcalendar/interaction';
+import frLocale from '@fullcalendar/core/locales/fr';
 
 interface AgendaCalendarProps {
     repairer: Repairer;
 }
 
 export const AgendaCalendar = ({repairer}: AgendaCalendarProps): JSX.Element => {
+
+
     return (
         <>
             <FullCalendar
-                plugins={[ dayGridPlugin ]}
-                initialView="dayGridMonth"
+                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+                initialView="timeGridDay"
                 weekends={false}
+                locales={[frLocale]}
+                locale="fr"
+                headerToolbar={{
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'timeGridDay,dayGridWeek,dayGridMonth',
+                }}
                 events={[
-                    { title: 'event 1', date: '2019-04-01' },
-                    { title: 'event 2', date: '2019-04-02' }
+                    { title: 'event 1', date: '2023-05-17' },
+                    { title: 'event 2', date: '2023-05-16' }
                 ]}
             />
         </>
