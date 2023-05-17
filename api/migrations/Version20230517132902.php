a76ccc610572f613b4a980321458ed220fad3c13
clementtalleu <clement.talleu@gmail.com>
2023-05-17 15:50:22 +0200

    add desc

--- api/migrations/Version20230517132902.php
+++ api/migrations/Version20230517132902.php
@@ -7,19 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230517132902 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Add bike infos in appointment table';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE appointment ADD bike_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE appointment ADD bike_type_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE appointment ADD CONSTRAINT FK_FE38F844D5A4816F FOREIGN KEY (bike_id) REFERENCES bike (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -30,7 +26,6 @@ final class Version20230517132902 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE appointment DROP CONSTRAINT FK_FE38F844D5A4816F');
         $this->addSql('ALTER TABLE appointment DROP CONSTRAINT FK_FE38F8447FF015AE');
