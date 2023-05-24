c4b2bba556b7b044070913c082cf7091f65f36d2
clementtalleu <clement.talleu@gmail.com>
2023-05-24 13:36:50 +0200

    add FAQ + mentions legales

---
+++ pwa/pages/faq.tsx
@@ -0,0 +1,76 @@
+import {NextPageWithLayout} from 'pages/_app';
+import React from 'react';
+import Head from 'next/head';
+import {
+    Container,
+    Typography,
+    Paper,
+} from '@mui/material';
+import WebsiteLayout from '@components/layout/WebsiteLayout';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const Faq: NextPageWithLayout = () => {
+    return (
+        <>
+            <Head>
+                <title>FAQ</title>
+            </Head>
+            <WebsiteLayout>
+                <Container sx={{width: {xs: '100%', md: '70%'}}}>
+                    <Paper elevation={4} sx={{maxWidth: 800, p: 4, mt: 4, mx: 'auto'}}>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1a-content"
+                                id="panel1a-header"
+                            >
+                                <Typography>Qui sommes nous ?</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2a-content"
+                                id="panel2a-header"
+                            >
+                                <Typography>Nous rejoindre</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2a-content"
+                                id="panel2a-header"
+                            >
+                                <Typography>Réparer son vélo</Typography>
+                            </AccordionSummary>
+                            <AccordionDetails>
+                                <Typography>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                </Typography>
+                            </AccordionDetails>
+                        </Accordion>
+                    </Paper>
+                </Container>
+            </WebsiteLayout>
+        </>
+    );
+};
+
+export default Faq;
