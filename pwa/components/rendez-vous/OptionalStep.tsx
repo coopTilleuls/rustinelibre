a3c9785d799e9d4c7d4c1b67809bd3e31fcc646d
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-05-24 11:50:28 +0200

    fix optionalpage content after an edition (#413)

--- pwa/components/rendez-vous/OptionalStep.tsx
+++ pwa/components/rendez-vous/OptionalStep.tsx
@@ -23,10 +23,14 @@ const OptionalStep = ({
       <Typography component="h2" fontSize={18} fontWeight={600} my={{xs: 2}}>
         À lire avant de prendre rendez-vous.
       </Typography>
-      <Typography paragraph fontSize={{xs: 16, md: 18}} color="text.secondary">
-        {optionalPage}
-      </Typography>
-
+      {optionalPage && (
+        <Typography
+          paragraph
+          fontSize={{xs: 16, md: 18}}
+          color="text.secondary"
+          dangerouslySetInnerHTML={{__html: optionalPage}}
+        />
+      )}
       <Typography fontSize={{xs: 16, md: 18, textAlign: 'center'}}>
         <Button variant="contained" onClick={confirmAppointmentRequest}>
           Suivant
