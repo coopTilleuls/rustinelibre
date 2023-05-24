8dc21c3266d358e1b430bc633d45022a2322c5a3
clementtalleu <clement.talleu@gmail.com>
2023-05-24 07:18:30 +0200

    fix dashboard access (#409)

---
+++ pwa/pages/dashboard/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Head from 'next/head';
+import Box from '@mui/material/Box';
+import DashboardLayout from '@components/dashboard/DashboardLayout';
+
+const Dashboard = () => {
+    return (
+        <>
+            <Head>
+                <title>Dashboard</title>
+            </Head>
+            <DashboardLayout>
+                <Box component="main">Hello tout le monde !</Box>
+            </DashboardLayout>
+        </>
+    );
+};
+
+export default Dashboard;
