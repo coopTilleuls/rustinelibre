ba85e8db421f9e09b1abea630d52fa2b7ec1ba7e
Davy <davy.beauzil72@gmail.com>
2023-05-25 14:39:24 +0200

    test: transition of appointment status

---
+++ api/tests/Appointments/Workflow/AppointmentStatusProposeAnotherSlotTest.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Appointments\Workflow;
+
+use App\Repository\AppointmentRepository;
+use App\Tests\AbstractTestCase;
+
+class AppointmentStatusProposeAnotherSlotTest extends AbstractTestCase
+{
+    private AppointmentRepository $appointmentRepository;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->appointmentRepository = self::getContainer()->get(AppointmentRepository::class);
+    }
+
+    public function testRepairerCanProposeAnotherSlotFromPendingRepairer(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'pending_repairer']);
+        $this->createClientWithUser($appointment->repairer->owner)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseIsSuccessful();
+        self::assertSame('pending_cyclist', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testRepairerCanProposeAnotherSlotFromPendingCyclist(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'pending_cyclist']);
+        $this->createClientWithUser($appointment->repairer->owner)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseIsSuccessful();
+        self::assertSame('pending_cyclist', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testRepairerCanProposeAnotherSlotFromValidated(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'validated']);
+        $this->createClientWithUser($appointment->repairer->owner)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseIsSuccessful();
+        self::assertSame('pending_cyclist', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testCyclistCanProposeAnotherSlotFromPendingRepairer(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'pending_repairer']);
+        $this->createClientWithUser($appointment->customer)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseIsSuccessful();
+        self::assertSame('pending_cyclist', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testCyclistCanProposeAnotherSlotFromPendingCyclist(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'pending_cyclist']);
+        $this->createClientWithUser($appointment->customer)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseIsSuccessful();
+        self::assertSame('pending_cyclist', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testCyclistCanProposeAnotherSlotFromValidated(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'validated']);
+        $this->createClientWithUser($appointment->customer)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseIsSuccessful();
+        self::assertSame('pending_cyclist', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testRepairerCannotProposeAnotherSlotFromRefused(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'refused']);
+        $this->createClientWithUser($appointment->repairer->owner)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseStatusCodeSame(400);
+        self::assertSame('refused', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testCyclistCannotProposeAnotherSlotFromRefused(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'refused']);
+        $this->createClientWithUser($appointment->customer)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseStatusCodeSame(400);
+        self::assertSame('refused', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testRepairerCannotProposeAnotherSlotFromCancel(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'cancel']);
+        $this->createClientWithUser($appointment->repairer->owner)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseStatusCodeSame(400);
+        self::assertSame('cancel', $this->appointmentRepository->find($appointment->id)->status);
+    }
+
+    public function testCyclistCannotProposeAnotherSlotFromCancel(): void
+    {
+        $appointment = $this->appointmentRepository->findOneBy(['status' => 'cancel']);
+        $this->createClientWithUser($appointment->customer)->request('PUT', sprintf('/appointment_status/%d', $appointment->id), [
+            'json' => [
+                'transition' => 'propose_another_slot',
+                'slotTime' => (new \DateTime('+1 day'))->format('Y-m-d H:i:s'),
+            ],
+        ]);
+        self::assertResponseStatusCodeSame(400);
+        self::assertSame('cancel', $this->appointmentRepository->find($appointment->id)->status);
+    }
+}
