e71704eeea71b5f64bab96b1e33b04d4c96a5d6a
Raphael-Marchese <103203222+Raphael-Marchese@users.noreply.github.com>
2023-06-01 14:50:50 +0200

    Add lastConnect property in User (#488)
    * Add lastConnect property in User
    
    Add property in User entity, make new migration and  event subscriber
    
    * add some tests of getting the last connection of an user
    
    * update after review
    
    new migration, modify lastConnect property, improve event subscriber

---
+++ api/src/User/EventSubscriber/LastConnectEventSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\User\EventSubscriber;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+final class LastConnectEventSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {
+    }
+
+    public function onLoginSuccess(InteractiveLoginEvent $event): void
+    {
+        $user = $event->getAuthenticationToken()->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->lastConnect = new \DateTimeImmutable('', new \DateTimeZone('Europe/Paris'));
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'security.interactive_login' => 'onLoginSuccess',
+        ];
+    }
+}
