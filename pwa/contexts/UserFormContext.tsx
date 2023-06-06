d43645e4fd181d3e63a218dd087876381adbf256
clementtalleu <clement.talleu@gmail.com>
2023-06-06 16:23:01 +0200

    add address and remove email/password from user edition (#521)

--- pwa/contexts/UserFormContext.tsx
+++ pwa/contexts/UserFormContext.tsx
@@ -13,6 +13,8 @@ interface UserFormContext {
     passwordError: boolean,
     emailHelperText: string,
     passwordInfo: string,
+    street: string,
+    city: string,
     setFirstName: (value: string) => void,
     setLastName: (value: string) => void,
     setEmail: (value: string) => void,
@@ -21,6 +23,8 @@ interface UserFormContext {
     setPasswordError: (value: boolean) => void,
     setEmailHelperText: (value: string) => void,
     setPasswordInfo: (value: string) => void,
+    setStreet: (value: string) => void,
+    setCity: (value: string) => void,
 }
 
 const initialValue = {
@@ -32,6 +36,8 @@ const initialValue = {
     passwordError: false,
     emailHelperText: '',
     passwordInfo: '',
+    street: '',
+    city: '',
     setFirstName: () => null,
     setLastName: () => null,
     setEmail: () => null,
@@ -40,6 +46,8 @@ const initialValue = {
     setPasswordError: () => null,
     setEmailHelperText: () => null,
     setPasswordInfo: () => null,
+    setStreet: () => null,
+    setCity: () => null,
 };
 
 export const UserFormContext = createContext<UserFormContext>(initialValue);
@@ -49,6 +57,8 @@ export const UserFormProvider = ({ children }: ProviderProps): JSX.Element => {
     const [lastName, setLastName] = useState<string>('');
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const [street, setStreet] = useState<string>('');
+    const [city, setCity] = useState<string>('');
     const [passwordError, setPasswordError] = useState<boolean>(false);
     const [emailError, setEmailError] = useState<boolean>(false);
     const [emailHelperText, setEmailHelperText] = useState<string>('');
@@ -65,6 +75,8 @@ export const UserFormProvider = ({ children }: ProviderProps): JSX.Element => {
                 passwordError,
                 emailHelperText,
                 passwordInfo,
+                street,
+                city,
                 setFirstName,
                 setLastName,
                 setEmail,
@@ -73,6 +85,8 @@ export const UserFormProvider = ({ children }: ProviderProps): JSX.Element => {
                 setPasswordError,
                 setEmailHelperText,
                 setPasswordInfo,
+                setStreet,
+                setCity,
             }}
         >
             {children}
