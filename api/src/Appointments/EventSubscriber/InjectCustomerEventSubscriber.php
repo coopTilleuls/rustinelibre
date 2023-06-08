445b7a86901353d49546491284251f283a7fe842
Raphael-Marchese <103203222+Raphael-Marchese@users.noreply.github.com>
2023-06-08 09:49:15 +0200

    Repairer cannot create appointment to non customer users (#515)
    * WIP repairer cannot create appointment to non customer users
    
    * fix tests and docker-compose.yml
    
    * update subscriber after review
    
    * improve InjectCustomerEventSubscriber and adapt some tests
    
    * php-cs-fixer

--- api/src/Appointments/EventSubscriber/InjectCustomerEventSubscriber.php
+++ api/src/Appointments/EventSubscriber/InjectCustomerEventSubscriber.php
@@ -7,15 +7,17 @@ namespace App\Appointments\EventSubscriber;
 use ApiPlatform\Symfony\EventListener\EventPriorities;
 use App\Entity\Appointment;
 use App\Entity\User;
+use App\Repository\AppointmentRepository;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 readonly class InjectCustomerEventSubscriber implements EventSubscriberInterface
 {
-    public function __construct(private Security $security)
+    public function __construct(private Security $security, private AppointmentRepository $appointmentRepository)
     {
     }
 
@@ -31,12 +33,22 @@ readonly class InjectCustomerEventSubscriber implements EventSubscriberInterface
         $object = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
-        if (!$object instanceof Appointment || Request::METHOD_POST !== $method) {
+        /** @var User $currentUser */
+        $currentUser = $this->security->getUser();
+
+        if (!$object instanceof Appointment || Request::METHOD_POST !== $method || ($this->security->isGranted('ROLE_ADMIN') && $object->customer && $currentUser->id !== $object->customer->id) || $object->customer && $currentUser->id === $object->customer->id) {
             return;
         }
 
-        /** @var User $currentUser */
-        $currentUser = $this->security->getUser();
+        if ($this->security->isGranted('ROLE_BOSS') || $this->security->isGranted('ROLE_EMPLOYEE')) {
+            $checkAppointment = $this->appointmentRepository->findOneBy([
+                'customer' => $object->customer->id,
+                'repairer' => $currentUser->repairerEmployee->repairer ?? $currentUser->repairer,
+            ]);
+            if (!$checkAppointment) {
+                throw new AccessDeniedHttpException('Cet utilisateur n\'est pas un de vos client');
+            }
+        }
         $object->customer = $currentUser;
     }
 }
