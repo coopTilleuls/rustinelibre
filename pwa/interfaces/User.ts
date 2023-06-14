1ffde3a46b470fee54cf4b026c81cc7cc3ab75cf
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-06-14 22:04:00 +0200

    adding last repairers on homepage (#564)

--- pwa/interfaces/User.ts
+++ pwa/interfaces/User.ts
@@ -1,18 +1,18 @@
-import {Repairer} from "@interfaces/Repairer";
+import {Repairer} from '@interfaces/Repairer';
 
 export interface User {
-    '@id': string;
-    '@type': string;
-    id: string;
-    firstName: string;
-    lastName: string;
-    city?: string;
-    street?: string;
-    email: string;
-    roles: string[];
-    repairer: Repairer,
-    plainPassword: string;
-    lastConnect?: string;
-    emailConfirmed: boolean;
-    lastRepairersf: Repairer[];
+  '@id': string;
+  '@type': string;
+  id: string;
+  firstName: string;
+  lastName: string;
+  city?: string;
+  street?: string;
+  email: string;
+  roles: string[];
+  repairer: Repairer;
+  plainPassword: string;
+  lastConnect?: string;
+  emailConfirmed: boolean;
+  lastRepairers: Repairer[];
 }
