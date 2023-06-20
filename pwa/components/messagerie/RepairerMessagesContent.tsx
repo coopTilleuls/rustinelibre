64b23e29b11fb6f5ae480f2a2f8950be5c443a9e
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-06-20 15:25:09 +0200

    messages chat components (#590)

---
+++ pwa/components/messagerie/RepairerMessagesContent.tsx
@@ -0,0 +1,171 @@
+import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
+import {useAccount} from '@contexts/AuthContext';
+import {ENTRYPOINT} from '@config/entrypoint';
+import {discussionMessageResource} from '@resources/discussionMessageResource';
+import {
+  Box,
+  Paper,
+  TextField,
+  Button,
+  CircularProgress,
+  Typography,
+} from '@mui/material';
+import {formatDate} from '@helpers/dateHelper';
+import {Discussion} from '@interfaces/Discussion';
+import {DiscussionMessage} from '@interfaces/DiscussionMessage';
+
+type MessagesContentProps = {
+  discussion: Discussion;
+};
+
+const RepairerMessagesContent = ({
+  discussion,
+}: MessagesContentProps): JSX.Element => {
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+  const {user} = useAccount({});
+  const [loading, setLoading] = useState<boolean>(false);
+  const [messages, setMessages] = useState<DiscussionMessage[]>([]);
+  const [messageToSend, setMessageToSend] = useState<string>('');
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const handleMessageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setMessageToSend(event.target.value);
+  };
+
+  const subscribeMercureDiscussion = async (): Promise<void> => {
+    const hubUrl = `${ENTRYPOINT}/.well-known/mercure`;
+    const hub = new URL(hubUrl);
+    hub.searchParams.append('topic', `${ENTRYPOINT}${discussion['@id']}`);
+    const eventSource = new EventSource(hub);
+    eventSource.onmessage = (event) => {
+      fetchMessages();
+    };
+  };
+
+  const handleSendMessage = async (): Promise<void> => {
+    if (!messageToSend) {
+      return;
+    }
+    setCurrentPage(1);
+    await discussionMessageResource.post({
+      content: messageToSend,
+      discussion: discussion['@id'],
+    });
+    setMessageToSend('');
+  };
+
+  const handleKeyDown = (event: any): void => {
+    if (event.keyCode === 13 && !event.shiftKey) {
+      event.preventDefault();
+      handleSendMessage();
+    }
+  };
+
+  const fetchMessages = async () => {
+    const response = await discussionMessageResource.getAll(true, {
+      discussion: discussion['@id'],
+      page: currentPage,
+    });
+
+    if (currentPage === 1) {
+      setMessages(response['hydra:member']);
+    } else {
+      const messagesToDisplay = messages as DiscussionMessage[];
+      response['hydra:member'].map((oldMessage) => {
+        messagesToDisplay.push(oldMessage);
+      });
+      setMessages(messagesToDisplay);
+    }
+  };
+
+  useEffect(() => {
+    if (messagesContainerRef.current) {
+      const messagesContainer = messagesContainerRef.current;
+      messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    }
+  }, [messages]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    setLoading(true);
+    fetchMessages();
+    subscribeMercureDiscussion();
+    setLoading(false);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    fetchMessages();
+  }, [currentPage, discussion]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <Box width="100%">
+      {loading && <CircularProgress />}
+      {!loading && user && (
+        <>
+          <Box
+            sx={{
+              top: 100,
+              pr: 2,
+              pb: 5,
+              height: '50vh',
+              overflowY: 'scroll',
+            }}
+            ref={messagesContainerRef}>
+            {messages
+              .sort((a, b) => a.id - b.id)
+              .map(({sender, content, createdAt}, index) => (
+                <Box
+                  key={index}
+                  width="70%"
+                  px={2}
+                  py={1}
+                  mb={2}
+                  borderRadius={2}
+                  textAlign={sender.id === user.id ? 'right' : 'left'}
+                  color={sender.id === user.id ? 'white' : 'black'}
+                  sx={{
+                    float: sender.id === user.id ? 'right' : 'left',
+                    backgroundColor:
+                      sender.id === user.id ? 'primary.main' : 'grey.200',
+                    wordBreak: 'break-all',
+                  }}>
+                  <>
+                    <Typography>{content}</Typography>
+                    <Typography fontSize={11} fontStyle="italic" pt={1}>
+                      {formatDate(createdAt)}
+                    </Typography>
+                  </>
+                </Box>
+              ))}
+          </Box>
+          <Box width="100%" mt={2}>
+            <Paper
+              elevation={4}
+              sx={{
+                padding: '1rem',
+                right: 5,
+              }}>
+              <TextField
+                label="Écrire un message"
+                fullWidth
+                multiline
+                rows={3}
+                value={messageToSend}
+                onChange={handleMessageChange}
+                onKeyDown={handleKeyDown}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{marginTop: '1rem', textTransform: 'capitalize'}}
+                onClick={handleSendMessage}>
+                Envoyer
+              </Button>
+            </Paper>
+          </Box>
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default RepairerMessagesContent;
--- pwa/pages/messagerie/index.tsx
+++ pwa/pages/messagerie/index.tsx
@@ -1,108 +1,30 @@
 import {NextPageWithLayout} from 'pages/_app';
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import Head from 'next/head';
-import Box from '@mui/material/Box';
-import {CircularProgress, Container} from '@mui/material';
-import {discussionResource} from "@resources/discussionResource";
-import {Discussion} from "@interfaces/Discussion";
-import MessagesContent from "@components/messagerie/MessagesContent";
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import {formatDate} from "@helpers/dateHelper";
-import WebsiteLayout from "@components/layout/WebsiteLayout";
-import {useAccount} from "@contexts/AuthContext";
-import {ENTRYPOINT} from "@config/entrypoint";
+import {Container, Typography} from '@mui/material';
+import WebsiteLayout from '@components/layout/WebsiteLayout';
+import CustomerDiscussionList from '@components/messagerie/CustomerDiscussionList';
 
-const Messagerie: NextPageWithLayout = () => {
-
-    const [discussions, setDiscussions] = useState<Discussion[]>([]);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [discussionSelected, setDiscussionSelected] = useState<Discussion | null>(null);
-    const {user} = useAccount({});
-
-    const subscribeMercureDiscussions = async(): Promise<void> => {
-        const hubUrl = `${ENTRYPOINT}/.well-known/mercure`
-        const hub = new URL(hubUrl);
-        discussions.map(discussion => {
-            hub.searchParams.append('topic', `${ENTRYPOINT}${discussion['@id']}`);
-        })
-
-        const eventSource = new EventSource(hub);
-        eventSource.onmessage = event => {
-            fetchDiscussions()
-        };
-    }
-
-    const fetchDiscussions = async() => {
-        if (!user) {
-            return;
-        }
-
-        setLoading(true);
-        const response = await discussionResource.getAll(true, {
-            'order[lastMessage]': 'DESC',
-            customer: user['@id']
-        });
-        setDiscussions(response['hydra:member']);
-        if (response['hydra:member'].length > 0) {
-            setDiscussionSelected(response['hydra:member'][0])
-        }
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        fetchDiscussions();
-    }, [user]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    useEffect(() => {
-        if (discussions.length > 0) {
-            subscribeMercureDiscussions();
-        }
-    }, [discussions]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    return (
-        <>
-            <Head>
-                <title>Messagerie</title>
-            </Head>
-            <WebsiteLayout>
-                <Container sx={{marginLeft: 0}}>
-                    <Box component="main" sx={{marginRight: '5%', marginTop: '100px'}}>
-
-                        {loading && <CircularProgress />}
-
-                        <Grid container spacing={4}>
-                            <Grid item md={3}>
-                                <Paper elevation={4}>
-                                    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-
-                                        {!loading && discussions.length === 0 && <Box sx={{margin: '10px'}}>
-                                            Pas de conversation engagées pour le moment
-                                        </Box>}
-
-                                        {discussions.length > 0 && discussions.map(discussion => {
-                                            return <ListItem key={discussion['@id']} onClick={() => setDiscussionSelected(discussion)} sx={{cursor: 'pointer'}}>
-                                                <ListItemText primary={discussion.repairer.name} secondary={formatDate(discussion.lastMessage, true)} />
-                                            </ListItem>
-                                        })}
-                                    </List>
-                                </Paper>
-                            </Grid>
-
-                            <Grid item md={8}>
-                                <Box sx={{position: 'relative', bottom: '100px'}}>
-                                    {discussionSelected && <MessagesContent discussion={discussionSelected} bottomPosition={80} />}
-                                </Box>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Container>
-            </WebsiteLayout>
-        </>
-    );
+const Messages: NextPageWithLayout = () => {
+  return (
+    <>
+      <Head>
+        <title>Messages</title>
+      </Head>
+      <WebsiteLayout>
+        <Container
+          sx={{
+            pt: 4,
+            width: {xs: '100%', md: '70%'},
+          }}>
+          <Typography fontSize={{xs: 28, md: 30}} fontWeight={600} pb={2}>
+            Messages
+          </Typography>
+          <CustomerDiscussionList />
+        </Container>
+      </WebsiteLayout>
+    </>
+  );
 };
 
-export default Messagerie;
+export default Messages;
