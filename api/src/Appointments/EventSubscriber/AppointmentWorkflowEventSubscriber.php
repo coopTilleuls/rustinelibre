4d0cd8b000aa82e97b80d54a932c6af4fcffee29
davy-beauzil <38990335+davy-beauzil@users.noreply.github.com>
2023-06-20 14:13:56 +0200

    add mailgun + some fixes (#592)

--- api/src/Appointments/EventSubscriber/AppointmentWorkflowEventSubscriber.php
+++ api/src/Appointments/EventSubscriber/AppointmentWorkflowEventSubscriber.php
@@ -133,7 +133,7 @@ readonly class AppointmentWorkflowEventSubscriber implements EventSubscriberInte
 
     private function sendAcceptanceEmail(Appointment $appointment): void
     {
-        if (in_array($this->kernel->getEnvironment(), ['dev', 'test'])) {
+        if ('test' === $this->kernel->getEnvironment()) {
             return;
         }
 
