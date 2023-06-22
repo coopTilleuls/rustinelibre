91a7550f096961b8f80f1a0c9e75d4134a01c0de
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-06-22 10:59:23 +0200

    fix/appointment-card

--- pwa/components/rendez-vous/AppointmentCard.tsx
+++ pwa/components/rendez-vous/AppointmentCard.tsx
@@ -1,78 +1,68 @@
 import React, {PropsWithRef} from 'react';
-import Link from 'next/link';
-import {
-    Stack,
-    Card,
-    CardContent,
-    CardMedia,
-    Typography,
-} from '@mui/material';
+import {Stack, Card, CardContent, CardMedia, Typography} from '@mui/material';
 import {apiImageUrl} from '@helpers/apiImagesHelper';
 import {formatDate} from 'helpers/dateHelper';
-import {Appointment} from "@interfaces/Appointment";
+import {Appointment} from '@interfaces/Appointment';
 
 interface AppointmentCardProps extends PropsWithRef<any> {
-    appointment: Appointment;
+  appointment: Appointment;
 }
 
-export const AppointmentCard = ({appointment}: AppointmentCardProps): JSX.Element => {
-    
-    return (
-        <Link href={`/reparateur/${appointment.repairer.id}`} style={{textDecoration: 'none'}}>
-            <Card
-                sx={{
-                    boxShadow: 0,
-                    border: (theme) => `4px solid ${theme.palette.grey[300]}`,
-                    display: 'flex',
-                }}
-            >
-                <CardMedia
-                    component="img"
-                    sx={{
-                        width: {xs: 50, md: 100},
-                        height: {xs: 50, md: 100},
-                        p: 2,
-                        borderRadius: '50%',
-                    }}
-                    image={
-                        appointment.repairer.thumbnail
-                            ? apiImageUrl(appointment.repairer.thumbnail.contentUrl)
-                            : 'https://cdn.cleanrider.com/uploads/2021/04/prime-reparation-velo_140920-3.jpg'
-                    }
-                    alt="Photo du réparateur"
-                />
-                <CardContent
-                    sx={{
-                        pl: 0,
-                        pr: 2,
-                        py: 2,
-                        flexGrow: 1,
-                        display: 'flex',
-                        flexDirection: 'column',
-                    }}>
-                    <Stack spacing={{xs: 1, md: 2}}>
-                        <div>
-                            <Typography
-                                fontSize={{xs: 16, md: 24}}
-                                fontWeight={600}
-                                sx={{wordBreak: 'break-word'}}>
-                                {appointment.repairer.name}
-                            </Typography>
-                        </div>
-                        <div>
-                            <Typography
-                                color="text.secondary"
-                                fontSize={{xs: 12, md: 16}}
-                                fontWeight={300}>
-                                Date du rendez-vous :
-                            </Typography>
-                            <Typography color="text.secondary" fontSize={{xs: 12, md: 16}}>
-                                <strong>{formatDate(appointment.slotTime)}</strong>
-                            </Typography>
-                        </div>
-                    </Stack>
-                </CardContent>
-            </Card>
-        </Link>
-    );
+export const AppointmentCard = ({
+  appointment,
+}: AppointmentCardProps): JSX.Element => {
+  return (
+    <Card
+      sx={{
+        boxShadow: 0,
+        border: (theme) => `4px solid ${theme.palette.grey[300]}`,
+        display: 'flex',
+      }}>
+      <CardMedia
+        component="img"
+        sx={{
+          width: {xs: 50, md: 100},
+          height: {xs: 50, md: 100},
+          p: 2,
+          borderRadius: '50%',
+        }}
+        image={
+          appointment.repairer.thumbnail
+            ? apiImageUrl(appointment.repairer.thumbnail.contentUrl)
+            : 'https://cdn.cleanrider.com/uploads/2021/04/prime-reparation-velo_140920-3.jpg'
+        }
+        alt="Photo du réparateur"
+      />
+      <CardContent
+        sx={{
+          pl: 0,
+          pr: 2,
+          py: 2,
+          flexGrow: 1,
+          display: 'flex',
+          flexDirection: 'column',
+        }}>
+        <Stack spacing={{xs: 1, md: 2}}>
+          <Typography
+            fontSize={{xs: 16, md: 24}}
+            fontWeight={600}
+            sx={{wordBreak: 'break-word'}}>
+            {appointment.repairer.name}
+          </Typography>
+          <Typography
+            color="text.secondary"
+            fontSize={{xs: 12, md: 16}}
+            fontWeight={300}>
+            Date du rendez-vous :
+          </Typography>
+          <Typography
+            color="text.secondary"
+            fontSize={{xs: 12, md: 16}}
+            fontWeight={700}>
+            {formatDate(appointment.slotTime)}
+          </Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 };
--- pwa/components/rendez-vous/MyFutureAppointments.tsx
+++ pwa/components/rendez-vous/MyFutureAppointments.tsx
@@ -1,62 +1,64 @@
 import React, {useEffect, useState} from 'react';
 import {Typography, Box} from '@mui/material';
-import {User} from "@interfaces/User";
-import {appointmentResource} from "@resources/appointmentResource";
-import {Appointment} from "@interfaces/Appointment";
-import {dateObjectAsString} from "@helpers/dateHelper";
-import {AppointmentCard} from "@components/rendez-vous/AppointmentCard";
+import {User} from '@interfaces/User';
+import {appointmentResource} from '@resources/appointmentResource';
+import {Appointment} from '@interfaces/Appointment';
+import {dateObjectAsString} from '@helpers/dateHelper';
+import {AppointmentCard} from '@components/rendez-vous/AppointmentCard';
 import Grid from '@mui/material/Grid';
 
 interface MyFutureAppointmentProps {
-    currentUser: User;
+  currentUser: User;
 }
 
 const MyFutureAppointment = ({currentUser}: MyFutureAppointmentProps) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
 
-    const [loading, setLoading] = useState<boolean>(false);
-    const [appointments, setAppointments] = useState<Appointment[]>([]);
+  const fetchAppointments = async () => {
+    setLoading(true);
+    const response = await appointmentResource.getAll(true, {
+      customer: currentUser.id,
+      'order[slotTime]': 'DESC',
+      'slotTime[after]': dateObjectAsString(new Date()),
+    });
 
-    const fetchAppointments = async () => {
-        setLoading(true);
-        const response = await appointmentResource.getAll(true, {
-            customer: currentUser.id,
-            'order[slotTime]': 'DESC',
-            'slotTime[after]': dateObjectAsString(new Date()),
-        });
+    setAppointments(response['hydra:member']);
+    setLoading(false);
+  };
 
-        setAppointments(response['hydra:member']);
-        setLoading(false);
-    }
+  useEffect(() => {
+    fetchAppointments();
+  }, [currentUser]); // eslint-disable-line react-hooks/exhaustive-deps
 
-    useEffect(() => {
-        fetchAppointments();
-    }, [currentUser]); // eslint-disable-line react-hooks/exhaustive-deps
-
-    return (
-        <Box>
-            <Typography variant="h4" sx={{mb: 2}}>
-                Rendez-vous à venir
+  return (
+    <Box>
+      <Typography variant="h4" sx={{mb: 2}}>
+        Rendez-vous à venir
+      </Typography>
+      <Box>
+        {!loading && appointments.length > 0 && (
+          <Grid container spacing={2}>
+            {!loading &&
+              appointments.map((appointment) => {
+                return (
+                  <Grid item xs={12} md={6} key={appointment.id}>
+                    <AppointmentCard appointment={appointment} />
+                  </Grid>
+                );
+              })}
+          </Grid>
+        )}
+        {!loading && appointments.length === 0 && (
+          <Box>
+            <Typography variant="body1">
+              Vous n&apos;avez pas de rendez vous à venir
             </Typography>
-            <Box>
-                {!loading && appointments.length > 0 &&
-                    <Grid container spacing={2}>
-                        {!loading && appointments.map(appointment => {
-                            return <Grid item xs={12} md={6} key={appointment.id}>
-                                <AppointmentCard appointment={appointment} />
-                            </Grid>
-                        })}
-                    </Grid>
-                }
-                {!loading && appointments.length === 0 &&
-                    <Box>
-                        <Typography variant="body1">
-                            Vous n&apos;avez pas de rendez vous à venir
-                        </Typography>
-                    </Box>
-                }
-            </Box>
-        </Box>
-    );
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
 };
 
 export default MyFutureAppointment;
