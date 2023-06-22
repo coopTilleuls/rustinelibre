514c31058447624b34dfe5ca60d669c373d60fc4
clementtalleu <clement.talleu@gmail.com>
2023-06-22 16:13:42 +0200

    add some infos in cards (#628)
    * add some infos in cards
    
    * refacto strong

--- pwa/components/rendez-vous/AppointmentCard.tsx
+++ pwa/components/rendez-vous/AppointmentCard.tsx
@@ -1,144 +1,115 @@
 import React, {PropsWithRef, useState} from 'react';
 import {
-  Stack,
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  Box,
-  Button,
-  CircularProgress,
+    Stack,
+    Card,
+    CardContent,
+    CardMedia,
+    Typography, Box, Button, CircularProgress,
 } from '@mui/material';
 import {apiImageUrl} from '@helpers/apiImagesHelper';
 import {formatDate} from 'helpers/dateHelper';
-import {Appointment} from '@interfaces/Appointment';
-import Link from 'next/link';
-import {appointmentResource} from '@resources/appointmentResource';
-import router from 'next/router';
+import {Appointment} from "@interfaces/Appointment";
+import Link from "next/link";
+import {appointmentResource} from "@resources/appointmentResource";
 
 interface AppointmentCardProps extends PropsWithRef<any> {
-  appointment: Appointment;
-  future: boolean;
-  fetchAppointments: () => void;
+    appointment: Appointment;
+    future: boolean;
+    fetchAppointments: () => void;
 }
 
-export const AppointmentCard = ({
-  appointment,
-  future,
-  fetchAppointments,
-}: AppointmentCardProps): JSX.Element => {
-  const [loading, setLoading] = useState<boolean>(false);
+export const AppointmentCard = ({appointment, future, fetchAppointments}: AppointmentCardProps): JSX.Element => {
 
-  const cancelAppointment = async (appointment: Appointment) => {
-    setLoading(true);
-    await appointmentResource.updateAppointmentStatus(appointment.id, {
-      transition: 'cancellation',
-    });
+    const [loading, setLoading] = useState<boolean>(false);
 
-    await fetchAppointments();
-    setLoading(false);
-  };
+    const cancelAppointment = async (appointment: Appointment) => {
 
-  return (
-    <Card
-      sx={{
-        boxShadow: 0,
-        border: (theme) => `4px solid ${theme.palette.grey[300]}`,
-        display: 'flex',
-      }}>
-      <CardMedia
-        component="img"
-        sx={{
-          width: {xs: 50, md: 100},
-          height: {xs: 50, md: 100},
-          p: 2,
-          borderRadius: '50%',
-        }}
-        image={
-          appointment.repairer.thumbnail
-            ? apiImageUrl(appointment.repairer.thumbnail.contentUrl)
-            : 'https://cdn.cleanrider.com/uploads/2021/04/prime-reparation-velo_140920-3.jpg'
-        }
-        alt="Photo du réparateur"
-      />
-      <CardContent
-        sx={{
-          pl: 0,
-          pr: 2,
-          py: 2,
-          flexGrow: 1,
-          display: 'flex',
-          flexDirection: 'column',
-        }}>
-        <Stack spacing={{xs: 1, md: 2}}>
-          <div>
-            <Typography
-              fontSize={{xs: 16, md: 24}}
-              fontWeight={600}
-              sx={{wordBreak: 'break-word'}}>
-              {formatDate(appointment.slotTime)}
-            </Typography>
-          </div>
-          <div>
-            <Typography
-              color="text.secondary"
-              fontSize={{xs: 12, md: 16}}
-              fontWeight={300}>
-              Réparateur :
-            </Typography>
-            <Typography color="text.secondary" fontSize={{xs: 12, md: 16}}>
-              <strong>{appointment.repairer.name}</strong>
-            </Typography>
-          </div>
-          <Box sx={{display: 'inline-flex'}}>
-            {future && (
-              <Box>
-                <Button
-                  variant="outlined"
-                  color={`warning`}
-                  size="small"
-                  onClick={() => cancelAppointment(appointment)}>
-                  {loading ? <CircularProgress /> : 'Annuler le RDV'}
-                </Button>
-                {!appointment.discussion && (
-                  <Button
-                    onClick={() => cancelAppointment(appointment)}
-                    disabled={true}
-                    variant="contained"
-                    size="small"
-                    sx={{marginLeft: '5px'}}>
-                    Envoyer un message
-                  </Button>
-                )}
-                {appointment.discussion && (
-                  <Link href={`/messagerie/${appointment.discussion.id}`}>
-                    <Button
-                      variant="contained"
-                      size="small"
-                      sx={{marginLeft: '5px'}}>
-                      Envoyer un message
-                    </Button>
-                  </Link>
-                )}
-              </Box>
-            )}
-            {!future && (
-              <Button
-                onClick={() =>
-                  router.push({
-                    pathname: `/reparateur/${appointment.repairer.id}`,
-                    query: {appointment: 1},
-                  })
+        setLoading(true);
+        await appointmentResource.updateAppointmentStatus(appointment.id, {
+            transition: 'cancellation'
+        });
+
+        await fetchAppointments();
+        setLoading(false);
+    }
+
+    return (
+        <Card
+            sx={{
+                boxShadow: 0,
+                border: (theme) => `4px solid ${theme.palette.grey[300]}`,
+                display: 'flex',
+                height: '200px'
+            }}
+        >
+            <CardMedia
+                component="img"
+                sx={{
+                    width: {xs: 50, md: 100},
+                    height: {xs: 50, md: 100},
+                    p: 2,
+                    borderRadius: '50%',
+                }}
+                image={
+                    appointment.repairer.thumbnail
+                        ? apiImageUrl(appointment.repairer.thumbnail.contentUrl)
+                        : 'https://cdn.cleanrider.com/uploads/2021/04/prime-reparation-velo_140920-3.jpg'
                 }
-                variant="outlined"
-                size="small"
-                sx={{marginLeft: '5px'}}>
-                Reprendre RDV
-              </Button>
-            )}
-          </Box>
-        </Stack>
-      </CardContent>
-    </Card>
-  );
+                alt="Photo du réparateur"
+            />
+            <CardContent
+                sx={{
+                    pl: 0,
+                    pr: 2,
+                    py: 2,
+                    flexGrow: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                }}>
+                <Stack spacing={{xs: 1, md: 2}}>
+                    <div>
+                        <Typography
+                            fontSize={{xs: 16, md: 24}}
+                            fontWeight={600}
+                            sx={{wordBreak: 'break-word'}}>
+                            {formatDate(appointment.slotTime)}
+                        </Typography>
+                    </div>
+                    <div>
+                        <Typography fontSize={{xs: 12, md: 16, fontWeight: 700}}>
+                            {appointment.repairer.name}
+                        </Typography>
+                        <Typography fontSize={{xs: 12, md: 16, fontWeight: 700}}>
+                            {appointment.repairer.streetNumber}
+                            {appointment.repairer.street},
+                            {appointment.repairer.postcode}
+                            {appointment.repairer.city}
+                        </Typography>
+                        {appointment.autoDiagnostic && <Typography color="text.secondary" fontSize={{xs: 12, md: 16, mt: 10}}>
+                            {appointment.autoDiagnostic.prestation}
+                            </Typography>
+                        }
+                    </div>
+                    <Box sx={{display: 'inline-flex'}}>
+                        {future && <Box>
+                            <Button variant="outlined" color={`warning`} size="small" onClick={() => cancelAppointment(appointment)}>
+                                {loading ? <CircularProgress /> : 'Annuler le RDV'}
+                            </Button>
+                            {!appointment.discussion && <Button onClick={() => cancelAppointment(appointment)} disabled={true} variant="contained" size="small" sx={{marginLeft: '5px'}}>
+                                Envoyer un message
+                            </Button>}
+                            {appointment.discussion && <Link href={`/messagerie/${appointment.discussion.id}`}><Button variant="contained" size="small" sx={{marginLeft: '5px'}}>
+                                Envoyer un message
+                            </Button></Link>}
+                        </Box>}
+                        {!future && <Box>
+                            <Link href={`/reparateur/${appointment.repairer.id}`}><Button variant="outlined" size="small" sx={{marginLeft: '5px'}}>
+                                Reprendre RDV
+                            </Button></Link>
+                        </Box>}
+                    </Box>
+                </Stack>
+            </CardContent>
+        </Card>
+    );
 };
