5548ab18d5251316243bf0c51e0d8ac3c6233b6d
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-06-23 16:00:25 +0200

    change repairer card pop for mobile (#648)

---
+++ pwa/components/repairers/PopUpRepairerCard.tsx
@@ -0,0 +1,55 @@
+import React, {PropsWithRef, useContext} from 'react';
+import {Box, Button, Typography} from '@mui/material';
+import {SearchRepairerContext} from '@contexts/SearchRepairerContext';
+
+import {Repairer} from '@interfaces/Repairer';
+import useMediaQuery from '@hooks/useMediaQuery';
+
+interface RepairerProps extends PropsWithRef<any> {
+  repairer: Repairer;
+  onClick?: () => void;
+}
+
+export const PopUpRepairerCard = ({
+  repairer,
+  onClick,
+}: RepairerProps): JSX.Element => {
+  const {selectedRepairer} = useContext(SearchRepairerContext);
+  const isMobile = useMediaQuery('(max-width: 640px)');
+
+  return (
+    <Box
+      sx={{
+        boxShadow: 0,
+        backgroundColor: 'white',
+      }}>
+      <Typography
+        my={0}
+        fontSize={14}
+        fontWeight={600}
+        sx={{wordBreak: 'break-word'}}>
+        {repairer.name}
+      </Typography>
+      <Typography
+        color="text.secondary"
+        textTransform="capitalize"
+        fontSize={12}>
+        {repairer.streetNumber} {repairer.street}
+      </Typography>
+      <Typography
+        color="text.secondary"
+        textTransform="capitalize"
+        fontSize={12}>
+        {repairer.postcode} - {repairer.city}
+      </Typography>
+      <Box textAlign="center" mt={2}>
+        <Button
+          variant="contained"
+          sx={{textTransform: 'capitalize'}}
+          onClick={onClick}>
+          Je réserve
+        </Button>
+      </Box>
+    </Box>
+  );
+};
