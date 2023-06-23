dafd8c48601cfbc48e66912426ced0eea7c855c6
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-06-23 15:52:40 +0200

    refacto changing password feature (#647)

--- pwa/components/layout/Footer.tsx
+++ pwa/components/layout/Footer.tsx
@@ -17,81 +17,78 @@ interface FooterProps {
   user?: User;
 }
 const Footer = ({user}: FooterProps): JSX.Element => {
-
   const router = useRouter();
   const isMobile = useMediaQuery('(max-width: 640px)');
 
   return (
-      <>
-        <Box
-          sx={{
-            position: 'fixed',
-            left: 0,
-            bottom: 0,
-            width: '100%',
-            zIndex: 10,
-          }}>
-          <Box
-            sx={{backgroundColor: 'grey.200'}}
-            py={{md: 1}}
-            width={'100%'}
-            height={'100%'}>
-            <BottomNavigation
-              showLabels
-              value={router.pathname}
-              sx={{backgroundColor: 'grey.200'}}>
-              <BottomNavigationAction
-                component={NextLinkComposed}
-                to={{pathname: '/'}}
-                label={!isMobile && 'Accueil'}
-                icon={<HomeIcon />}
-                value="/"
-              />
-              <BottomNavigationAction
-                component={NextLinkComposed}
-                to={{pathname: '/rendez-vous/mes-rendez-vous'}}
-                label={!isMobile && 'Rendez-vous'}
-                icon={<CalendarMonthIcon />}
-              />
-              <BottomNavigationAction
-                component={NextLinkComposed}
-                to={{pathname: '/velos/mes-velos'}}
-                label={!isMobile && 'Mes vélos'}
-                icon={<DirectionsBikeIcon />}
-              />
-              {!isBoss && !isEmployee && (
-                <BottomNavigationAction
-                  component={NextLinkComposed}
-                  to={{pathname: user ? '/messagerie' : '/'}}
-                  label={!isMobile && 'Messages'}
-                  icon={<ChatBubbleIcon />}
-                  disabled={!user}
-                  sx={{
-                    opacity: user ? 1 : 0.5,
-                  }}
-                />
-              )}
-              {user ? (
-                <BottomNavigationAction
-                  component={NextLinkComposed}
-                  to={{pathname: '/profil/mon-profil'}}
-                  label={!isMobile && 'Profil'}
-                  icon={<AccountCircleIcon />}
-                  value="/profil/mon-profil"
-                />
-              ) : (
-                <BottomNavigationAction
-                  component={NextLinkComposed}
-                  to={{pathname: '/login'}}
-                  label={!isMobile && 'Connexion'}
-                  icon={<AccountCircleIcon />}
-                  value="/login"
-                />
-              )}
-            </BottomNavigation>
-          </Box>
-        </Box>
-      </>
+    <Box
+      sx={{
+        position: 'fixed',
+        left: 0,
+        bottom: 0,
+        width: '100%',
+        zIndex: 10,
+      }}>
+      <Box
+        sx={{backgroundColor: 'grey.200'}}
+        py={{md: 1}}
+        width={'100%'}
+        height={'100%'}>
+        <BottomNavigation
+          showLabels
+          value={router.pathname}
+          sx={{backgroundColor: 'grey.200'}}>
+          <BottomNavigationAction
+            component={NextLinkComposed}
+            to={{pathname: '/'}}
+            label={!isMobile && 'Accueil'}
+            icon={<HomeIcon />}
+            value="/"
+          />
+          <BottomNavigationAction
+            component={NextLinkComposed}
+            to={{pathname: '/rendez-vous/mes-rendez-vous'}}
+            label={!isMobile && 'Rendez-vous'}
+            icon={<CalendarMonthIcon />}
+          />
+          <BottomNavigationAction
+            component={NextLinkComposed}
+            to={{pathname: '/velos/mes-velos'}}
+            label={!isMobile && 'Mes vélos'}
+            icon={<DirectionsBikeIcon />}
+          />
+          {!isBoss && !isEmployee && (
+            <BottomNavigationAction
+              component={NextLinkComposed}
+              to={{pathname: user ? '/messagerie' : '/'}}
+              label={!isMobile && 'Messages'}
+              icon={<ChatBubbleIcon />}
+              disabled={!user}
+              sx={{
+                opacity: user ? 1 : 0.5,
+              }}
+            />
+          )}
+          {user ? (
+            <BottomNavigationAction
+              component={NextLinkComposed}
+              to={{pathname: '/mon-compte'}}
+              label={!isMobile && 'Compte'}
+              icon={<AccountCircleIcon />}
+              value="/mon-compte"
+            />
+          ) : (
+            <BottomNavigationAction
+              component={NextLinkComposed}
+              to={{pathname: '/login'}}
+              label={!isMobile && 'Connexion'}
+              icon={<AccountCircleIcon />}
+              value="/login"
+            />
+          )}
+        </BottomNavigation>
+      </Box>
+    </Box>
   );
 };
 
