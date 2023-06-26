f00d5162a536fde454c96ae77bcdeb2dc76dfbeb
clementtalleu <clement.talleu@gmail.com>
2023-06-26 11:14:41 +0200

    serial groups contact (#655)

--- api/src/Entity/Contact.php
+++ api/src/Entity/Contact.php
@@ -16,10 +16,14 @@ use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
 use App\Repository\ContactRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => [self::READ]],
+    denormalizationContext: ['groups' => [self::WRITE]]
+)]
 #[Get(security: "is_granted('ROLE_ADMIN')")]
 #[GetCollection(security: "is_granted('ROLE_ADMIN')")]
 #[Post]
@@ -29,10 +33,14 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ApiFilter(BooleanFilter::class, properties: ['alreadyRead'])]
 class Contact
 {
+    public const READ = 'contact_read';
+    public const WRITE = 'contact_write';
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     #[ApiProperty(identifier: true)]
+    #[Groups([self::READ])]
     public ?int $id = null;
 
     #[Assert\NotBlank]
@@ -41,6 +49,7 @@ class Contact
         max : 100,
     )]
     #[ORM\Column(length: 100)]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $firstName = null;
 
     #[Assert\NotBlank]
@@ -49,6 +58,7 @@ class Contact
         max : 100,
     )]
     #[ORM\Column(length: 100)]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $lastName = null;
 
     #[Assert\Email]
@@ -58,6 +68,7 @@ class Contact
         max : 100,
     )]
     #[ORM\Column(length: 100)]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $email = null;
 
     #[Assert\NotBlank]
@@ -66,11 +77,13 @@ class Contact
         max : 1000,
     )]
     #[ORM\Column(length: 1000)]
+    #[Groups([self::READ, self::WRITE])]
     public ?string $content = null;
 
     #[Assert\NotNull]
     #[Assert\Type('boolean')]
     #[ORM\Column]
+    #[Groups([self::READ, self::WRITE])]
     public ?bool $alreadyRead = false;
 
     #[Assert\NotNull]
