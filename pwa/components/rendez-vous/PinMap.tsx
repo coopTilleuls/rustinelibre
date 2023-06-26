b88808a554a399ba7bbca0a1c76857d118a9636d
clementtalleu <clement.talleu@gmail.com>
2023-06-26 14:19:22 +0200

    add street + number + city in itinerant appointment (#656)

--- pwa/components/rendez-vous/PinMap.tsx
+++ pwa/components/rendez-vous/PinMap.tsx
@@ -1,7 +1,11 @@
-import React, {ChangeEvent} from 'react';
-import {Box, Typography, Button, TextField} from '@mui/material';
+import React, {ChangeEvent, useCallback, useEffect, useState} from 'react';
+import {Box, Typography, Button, TextField, Autocomplete} from '@mui/material';
 import {Repairer} from '@interfaces/Repairer';
 import MapPositionUser from '@components/dashboard/appointments/MapPositionUser';
+import {City as GouvCity} from "@interfaces/Gouv";
+import {searchCity, searchStreet} from "@utils/apiCity";
+import {createCitiesWithGouvAPI, City} from "@interfaces/City";
+import {Street} from "@interfaces/Street";
 
 interface PinMapProps {
   repairer: Repairer;
@@ -27,8 +31,63 @@ const PinMap = ({
   setAddress
 }: PinMapProps): JSX.Element => {
 
-  const handleChangeAddress = (event: ChangeEvent<HTMLInputElement>) => {
-    setAddress(event.target.value);
+  const [city, setCity] = useState<City | null>(null);
+  const [street, setStreet] = useState<string | null>(null);
+  const [streetNumber, setStreetNumber] = useState<string | null>(null);
+  const [latitudeCalculate, setLatitudeCalculate] = useState<string | null>(null);
+  const [longitudeCalculate, setLongitudeCalculate] = useState<string | null>(null);
+  const [cityInput, setCityInput] = useState<string>('');
+  const [citiesList, setCitiesList] = useState<City[]>([]);
+  const [streetList, setStreetList] = useState<Street[]>([]);
+  const [streetSelected, setStreetSelected] = useState<Street|null>(null);
+
+
+  const handleChangeStreetNumber = (event: ChangeEvent<HTMLInputElement>) => {
+      setStreetNumber(event.target.value);
+  };
+
+  const handleChangeStreet = async (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): Promise<void> => {
+        const adresseSearch = event.target.value;
+        if (adresseSearch.length >= 3) {
+            const streetApiResponse = await searchStreet(adresseSearch, city);
+            setStreetList(streetApiResponse)
+        }
+  };
+
+  const fetchCitiesResult = useCallback(
+      async (cityStr: string) => {
+        const citiesResponse = await searchCity(cityStr, false);
+        const cities: City[] =  createCitiesWithGouvAPI(citiesResponse as GouvCity[]);
+        setCitiesList(cities);
+      },
+      [setCitiesList]
+  );
+
+  useEffect(() => {
+    if (cityInput === '' || cityInput.length < 3) {
+      setCitiesList([]);
+    } else fetchCitiesResult(cityInput);
+  }, [setCitiesList, fetchCitiesResult, cityInput]);
+
+
+  useEffect(() => {
+    if (streetSelected) {
+        setLatitudeCalculate(streetSelected.lat.toString());
+        setLongitudeCalculate(streetSelected.lon.toString());
+    }
+  }, [streetSelected]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    if (streetNumber) {
+        setAddress(`${streetNumber} ${streetSelected?.name}, ${city?.name} (${city?.postcode})`)
+    }
+  }, [streetNumber]); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+  const handleCityChange = async (
+      event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ): Promise<void> => {
+    setCityInput(event.target.value);
   };
 
   return (
@@ -37,29 +96,78 @@ const PinMap = ({
         Placez le repère sur le lieu où vous souhaitez l&apos;intervention du
         réparateur.
       </Typography>
-      <MapPositionUser
-        repairer={repairer}
-        setLatitude={setLatitude}
-        setLongitude={setLongitude}
+
+      <Autocomplete
+          sx={{mt: 2, mb: 1}}
+          freeSolo
+          value={cityInput}
+          options={citiesList}
+          getOptionLabel={(city) =>
+              typeof city === 'string'
+                  ? city
+                  : `${city.name} (${city.postcode})`
+          }
+          onChange={(event, value) => setCity(value as City)}
+          renderInput={(params) => (
+              <TextField
+                  label="Ville"
+                  required
+                  {...params}
+                  value={cityInput}
+                  onChange={(e) => handleCityChange(e)}
+              />
+          )}
       />
-      {latitude && longitude && <TextField
+
+      {city && <Autocomplete
+          sx={{mt: 2, mb: 1}}
+          freeSolo
+          value={street}
+          options={streetList}
+          getOptionLabel={(streetObject) =>
+              typeof streetObject === 'string'
+                  ? streetObject
+                  :`${streetObject.name} (${streetObject.city})`
+          }
+          onChange={(event, value) => setStreetSelected(value as Street)}
+          renderInput={(params) => (
+              <TextField
+                  label="Rue"
+                  required
+                  {...params}
+                  value={street}
+                  onChange={(e) => handleChangeStreet(e)}
+              />
+          )}
+      />}
+
+      {streetSelected && <TextField
           margin="normal"
           required
           fullWidth
           id="address"
-          label="Indiquez votre adresse et les informations nécessaires pour le réparateur"
+          label="Indiquez votre numéro de rue et les informations nécessaires afin de permettre au réparateur de trouver votre adresse"
           name="address"
           autoComplete="address"
-          value={address}
+          value={streetNumber}
           inputProps={{ maxLength: 250 }}
-          onChange={handleChangeAddress}
+          onChange={handleChangeStreetNumber}
       />}
+
+      {city && streetSelected && streetNumber && latitudeCalculate && longitudeCalculate && <MapPositionUser
+        repairer={repairer}
+        setLatitude={setLatitude}
+        setLongitude={setLongitude}
+        latitudeProps={latitudeCalculate}
+        longitudeProps={longitudeCalculate}
+      />}
+
       <Box display="flex" justifyContent="space-between" px={2} pt={2}>
         <Button variant="outlined" onClick={cancelPinMap}>
           Retour
         </Button>
         <Button
-          disabled={!longitude || !latitude || !address}
+          disabled={!longitude || !latitude || !city || !streetSelected || !streetNumber}
           variant="contained"
           onClick={confirmPinMap}>
           Suivant
