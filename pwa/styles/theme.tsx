5c71d7c98f8ce918960fdfd180460cc9b5b1ed29
Laury S <ginifizz@users.noreply.github.com>
2023-07-04 10:13:06 +0200

    feat: start material theme (#707)

--- pwa/styles/theme.tsx
+++ pwa/styles/theme.tsx
@@ -1,17 +1,90 @@
-import {createTheme} from '@mui/material/styles';
+import {
+  Shadows,
+  SimplePaletteColorOptions,
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material/styles';
+import '@fontsource-variable/pathway-extreme';
+
+declare module '@mui/material/styles' {
+  // eslint-disable-next-line no-unused-vars
+  interface Palette {
+    white: SimplePaletteColorOptions;
+    black: SimplePaletteColorOptions;
+    cream: SimplePaletteColorOptions;
+  }
+  // eslint-disable-next-line no-unused-vars
+  interface PaletteOptions {
+    white?: SimplePaletteColorOptions;
+    cream?: SimplePaletteColorOptions;
+    black?: SimplePaletteColorOptions;
+  }
+}
 
 const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ee721e',
+    },
+    secondary: {
+      main: '#55267a',
+    },
+    cream: {
+      main: '#fff7f1',
+    },
+  },
+  typography: {
+    fontFamily: 'Pathway Extreme Variable',
+    h1: {
+      fontWeight: 900,
+      fontSize: 56,
+    },
+    h2: {
+      fontWeight: 900,
+      fontSize: 44,
+    },
+    h3: {
+      fontWeight: 800,
+      fontSize: 32,
+    },
+    h4: {
+      fontWeight: 700,
+      fontSize: 26,
+    },
+  },
+  shadows: [
+    'none',
+    '0 2px 4px rgba(0, 0, 0, 0.05), 0 4px 8px rgba(0, 0, 0, 0.05)',
+    '0 3px 6px rgba(0, 0, 0, 0.08), 0 6px 12px rgba(0, 0, 0, 0.08)',
+    '0 4px 8px rgba(0, 0, 0, 0.06), 0 8px 16px rgba(0, 0, 0, 0.06)',
+    '0 5px 10px rgba(0, 0, 0, 0.05), 0 10px 20px rgba(0, 0, 0, 0.05)',
+    '0 6px 12px rgba(0, 0, 0, 0.04), 0 12px 24px rgba(0, 0, 0, 0.04)',
+    ...Array(20).fill('none'),
+  ] as Shadows,
   components: {
     MuiButton: {
-      variants: [
-        {
-          props: {},
-          style: {
-            borderRadius: 20,
-            fontWeight: 600,
-          },
-        },
-      ],
+      defaultProps: {
+        disableElevation: true,
+      },
+      styleOverrides: {
+        root: ({ownerState}) => ({
+          borderRadius: 30,
+          fontWeight: 600,
+          textTransform: 'none',
+          ...(ownerState.variant === 'contained' && {
+            color: '#fff',
+          }),
+          ...(ownerState.size === 'large' && {
+            padding: '10px 28px',
+          }),
+          ...(ownerState.size === 'medium' && {
+            padding: '6px 24px',
+          }),
+          ...(ownerState.size === 'small' && {
+            padding: '4px 12px',
+          }),
+        }),
+      },
     },
     MuiBottomNavigationAction: {
       styleOverrides: {
@@ -26,4 +99,4 @@ const theme = createTheme({
   },
 });
 
-export default theme;
+export default responsiveFontSizes(theme);
