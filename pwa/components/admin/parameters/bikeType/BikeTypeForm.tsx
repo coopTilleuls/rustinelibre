a0c6753c0096cb355d0daf9adb890916faa7ec63
Raphael-Marchese <103203222+Raphael-Marchese@users.noreply.github.com>
2023-07-05 11:34:31 +0200

    Add error messages on bike type forms (#719)
    Display error messages from api and lint files

--- pwa/components/admin/parameters/bikeType/BikeTypeForm.tsx
+++ pwa/components/admin/parameters/bikeType/BikeTypeForm.tsx
@@ -1,93 +1,101 @@
-import React, {useState, useEffect, useContext, ChangeEvent} from 'react';
+import React, {useState, useEffect, ChangeEvent} from 'react';
 import Box from '@mui/material/Box';
-import {Alert, Button, CircularProgress, Container, Link, Paper, TextField, Typography} from '@mui/material';
-import {BikeType} from "@interfaces/BikeType";
-import {bikeTypeResource} from "@resources/bikeTypeResource";
-import {useRouter} from "next/router";
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+} from '@mui/material';
+import {BikeType} from '@interfaces/BikeType';
+import {bikeTypeResource} from '@resources/bikeTypeResource';
+import {useRouter} from 'next/router';
+import {errorRegex} from '@utils/errorRegex';
 
 type BikeTypeFormProps = {
-    bikeType: BikeType|null;
+  bikeType: BikeType | null;
 };
 
 const BikeTypeForm = ({bikeType}: BikeTypeFormProps): JSX.Element => {
+  const router = useRouter();
+  const [name, setName] = useState<string>('');
+  const [pending, setPending] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-    const router = useRouter();
-    const [name, setName] = useState<string>('');
-    const [pending, setPending] = useState<boolean>(false);
-    const [success, setSuccess] = useState<boolean>(false);
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  useEffect(() => {
+    if (bikeType) {
+      setName(bikeType.name);
+    }
+  }, [bikeType]);
 
-    useEffect(() => {
-        if (bikeType) {
-            setName(bikeType.name)
-        }
-    }, [bikeType]);
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setErrorMessage(null);
+    setPending(true);
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setErrorMessage(null);
-        setPending(true);
+    try {
+      if (bikeType) {
+        await bikeTypeResource.put(bikeType['@id'], {
+          name: name,
+        });
+        setSuccess(true);
+        setTimeout(() => {
+          setSuccess(false);
+        }, 3000);
+      } else {
+        await bikeTypeResource.post({
+          name: name,
+        });
+        router.push('/admin/parametres');
+      }
+    } catch (e:any) {
+      setErrorMessage(e.message?.replace(errorRegex, '$2'))
+    }
 
-        try {
-            if (bikeType) {
-                await bikeTypeResource.put(bikeType['@id'], {
-                    'name': name
-                })
-                setSuccess(true);
-                setTimeout(() => {setSuccess(false)}, 3000);
-            } else {
-                await bikeTypeResource.post({
-                    'name': name
-                })
-                router.push('/admin/parametres')
-            }
-        } catch {
+    setPending(false);
+  };
 
-        }
+  const handleChangeName = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
 
-        setPending(false);
-    };
+  return (
+    <Box component="form" onSubmit={handleSubmit} noValidate sx={{mt: 1}}>
+      <TextField
+        margin="normal"
+        required
+        fullWidth
+        id="email"
+        label="Nom"
+        name="name"
+        autoComplete="name"
+        autoFocus
+        value={name}
+        inputProps={{maxLength: 180}}
+        onChange={handleChangeName}
+      />
 
-    const handleChangeName = (event: ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-    };
+      <Box display="flex" flexDirection="column" alignItems="center">
+        <Button type="submit" variant="contained" sx={{my: 2}}>
+          {pending && <CircularProgress sx={{color: 'white'}} />}
+          {!pending && bikeType && 'Modifier le nom'}
+          {!pending && !bikeType && 'Ajouter ce type de vélo'}
+        </Button>
+        {errorMessage && (
+          <Typography variant="body1" color="error">
+            {errorMessage}
+          </Typography>
+        )}
+      </Box>
 
-    return (
-            <Box
-                component="form"
-                onSubmit={handleSubmit}
-                noValidate
-                sx={{mt: 1}}>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Nom"
-                    name="name"
-                    autoComplete="name"
-                    autoFocus
-                    value={name}
-                    inputProps={{ maxLength: 180 }}
-                    onChange={handleChangeName}
-                />
-
-                <Box display="flex" flexDirection="column" alignItems="center">
-                    <Button type="submit" variant="contained" sx={{my: 2}}>
-                        {pending && <CircularProgress sx={{color: 'white'}} />}
-                        {!pending && bikeType && 'Modifier le nom'}
-                        {!pending && !bikeType && 'Ajouter ce type de vélo'}
-                    </Button>
-                    {errorMessage && (
-                        <Typography variant="body1" color="error">
-                            {errorMessage}
-                        </Typography>
-                    )}
-                </Box>
-
-                {success && <Alert sx={{marginTop: '10px'}} severity="success">Vélo mis à jour</Alert>}
-            </Box>
-    );
+      {success && (
+        <Alert sx={{marginTop: '10px'}} severity="success">
+          Vélo mis à jour
+        </Alert>
+      )}
+    </Box>
+  );
 };
 
 export default BikeTypeForm;
