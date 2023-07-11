17ce3eee3d292d6aefbc4f40e9abd07b5b06e803
clementtalleu <clement.talleu@gmail.com>
2023-07-11 09:22:34 +0200

    remove JSW useless import

--- pwa/components/notifications/Notifications.tsx
+++ pwa/components/notifications/Notifications.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useState, JSX} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import * as React from 'react';
 import {FirebaseApp} from 'firebase/app';
 import {onMessage, Messaging, NotificationPayload} from 'firebase/messaging';
