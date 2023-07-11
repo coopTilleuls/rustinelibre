c70884890732e92eb2ca71637bc3d224af89bc18
clementtalleu <clement.talleu@gmail.com>
2023-07-11 10:27:30 +0200

    refacto

--- api/src/Notifications/Notification.php
+++ api/src/Notifications/Notification.php
@@ -10,22 +10,7 @@ class Notification
 {
     public const DEFAULT_ICON = 'https://cdn-icons-png.flaticon.com/512/565/565422.png';
 
-    public User $recipient;
-    public string $title;
-    public string $body;
-    public string $color;
-    public string $image;
-    public string $icon;
-    public array $params = [];
-
-    public function __construct(User $recipient, string $title, string $body, ?string $color = '#f45342', ?string $icon = self::DEFAULT_ICON, ?string $image = self::DEFAULT_ICON, ?array $params = [])
+    public function __construct(public User $recipient, public string $title, public string $body, public ?string $color = '#f45342', public ?string $icon = self::DEFAULT_ICON, public ?string $image = self::DEFAULT_ICON, public ?array $params = [])
     {
-        $this->recipient = $recipient;
-        $this->title = $title;
-        $this->body = $body;
-        $this->color = $color;
-        $this->icon = $icon;
-        $this->image = $image;
-        $this->params = $params;
     }
 }
