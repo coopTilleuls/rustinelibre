6cb2857fb0924e9de2b0f9b03d4702052c7e1aec
clementtalleu <clement.talleu@gmail.com>
2023-07-13 16:04:41 +0200

    refacto emails

--- api/src/User/EventSubscriber/ValidationCodeSubscriber.php
+++ api/src/User/EventSubscriber/ValidationCodeSubscriber.php
@@ -4,23 +4,17 @@ declare(strict_types=1);
 
 namespace App\User\EventSubscriber;
 
+use App\Emails\ValidationCodeEmail;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs as BaseLifecycleEventArgs;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Twig\Environment;
 
 #[AsDoctrineListener(event: Events::prePersist)]
 #[AsDoctrineListener(event: Events::postPersist)]
 final class ValidationCodeSubscriber
 {
-    public function __construct(private MailerInterface $mailer,
-        private string $mailerSender,
-        private LoggerInterface $logger,
-        private Environment $twig)
+    public function __construct(private ValidationCodeEmail $validationCodeEmail)
     {
     }
 
@@ -43,23 +37,6 @@ final class ValidationCodeSubscriber
             return;
         }
 
-        $this->sendValidationCodeByEmail($entity);
-    }
-
-    private function sendValidationCodeByEmail(User $user): void
-    {
-        $email = (new Email())
-            ->from($this->mailerSender)
-            ->to($user->email)
-            ->subject('Votre code de confirmation La Rustine Libre')
-            ->html($this->twig->render('mail/send_validation_code.html.twig', [
-                'user' => $user,
-            ]));
-
-        try {
-            $this->mailer->send($email);
-        } catch (\Exception $e) {
-            $this->logger->alert(sprintf('Confirmation code not send, error: %s', $e->getMessage()));
-        }
+        $this->validationCodeEmail->sendValidationCodeEmail($entity);
     }
 }
