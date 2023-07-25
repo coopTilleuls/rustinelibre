318fae443feb9a73b1d9592136dd73c37c22e7ce
clementtalleu <clement.talleu@gmail.com>
2023-07-25 09:55:47 +0200

    change naming button edit repairer

--- pwa/components/dashboard/informations/OptionalInfos.tsx
+++ pwa/components/dashboard/informations/OptionalInfos.tsx
@@ -71,7 +71,7 @@ export const OptionalInfos = ({
       <Editor content={optionalPage} setContent={setOptionalPage} />
       <Button type="submit" variant="contained" sx={{my: 2}}>
         {!pendingRegistration ? (
-          'Enregistrer mes informations'
+          'Enregistrer les informations'
         ) : (
           <CircularProgress size={20} sx={{color: 'white'}} />
         )}
