b4c086b1d9ed604750a313a660376ae8e5a2e827
Davy <davy.beauzil72@gmail.com>
2023-07-25 12:00:09 +0200

    add different message for repairer and cyclist

--- pwa/components/messagerie/NoMessageListItem.tsx
+++ pwa/components/messagerie/NoMessageListItem.tsx
@@ -1,6 +1,11 @@
 import {Box, Typography} from '@mui/material';
+import React from 'react';
+import {useAccount} from '@contexts/AuthContext';
+import {isBoss, isEmployee} from '@helpers/rolesHelpers';
 
 const NoMessageListItem = (): JSX.Element => {
+  const {user} = useAccount({redirectIfNotFound: '/login'});
+
   return (
     <Box
       sx={{
@@ -11,14 +16,35 @@ const NoMessageListItem = (): JSX.Element => {
         transition: 'all ease 0.3s',
         bgcolor: 'grey.100',
       }}>
-      <Box px={2} py={2} display="flex" gap={2} alignItems="center">
+      <Box px={2} py={2}>
         <Typography
           variant="body2"
           fontWeight={800}
           gutterBottom
           color={'text.secondary'}>
-          Vous n’avez pas encore envoyé de message
+          Vous n’avez pas de discussion en cours
         </Typography>
+
+        {user && (
+          <Typography
+            color="grey.500"
+            variant="caption"
+            fontStyle="italic"
+            component="div"
+            lineHeight="1.2">
+            {isBoss(user) || isEmployee(user) ? (
+              <>
+                Vos discussions apparaîtront ici quand un utilisateur aura pris
+                rendez-vous avec vous
+              </>
+            ) : (
+              <>
+                Vos discussions apparaîtront ici quand vous prendrez rendez-vous
+                avec un réparateur
+              </>
+            )}
+          </Typography>
+        )}
       </Box>
     </Box>
   );
