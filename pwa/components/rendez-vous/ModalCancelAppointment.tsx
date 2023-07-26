504536b32a663a6b4ffd8ef9e79c05a92ae884c0
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-07-26 10:16:06 +0200

    refacto add maintenance modal

--- pwa/components/rendez-vous/ModalCancelAppointment.tsx
+++ pwa/components/rendez-vous/ModalCancelAppointment.tsx
@@ -1,57 +1,88 @@
-import React, {useState} from 'react';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import {Button, CircularProgress, Typography} from '@mui/material';
+import React from 'react';
+import {
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Typography,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import {Appointment} from '@interfaces/Appointment';
+import {formatDate} from '@helpers/dateHelper';
 
 type ModalCancelAppointmentProps = {
+  appointment: Appointment;
   openModal: boolean;
   loading: boolean;
+  errorMessage: string | null;
   handleCloseModal: () => void;
   handleCancelAppointment: () => void;
 };
 
 const ModalCancelAppointment = ({
+  appointment,
   openModal,
   loading,
+  errorMessage,
   handleCloseModal,
   handleCancelAppointment,
 }: ModalCancelAppointmentProps): JSX.Element => {
   return (
-    <Modal
+    <Dialog
+      maxWidth="sm"
+      fullWidth
       open={openModal}
       onClose={handleCloseModal}
-      aria-labelledby="Ajouter un vélo"
-      aria-describedby="popup_add_bike">
-      <Box
-        position={'absolute'}
-        top={'50%'}
-        left={'50%'}
-        width={{xs: '85%', md: '40%'}}
-        maxWidth={500}
-        p={4}
-        boxShadow={24}
+      aria-labelledby="Annuler un rendez-vous"
+      aria-describedby="modal_cancel_appointment">
+      <DialogTitle
         sx={{
-          backgroundColor: 'background.paper',
-          transform: 'translate(-50%, -50%)',
+          m: 0,
+          p: 1,
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
         }}>
-        <Typography component="h2" fontWeight={600} textAlign="center">
-          Etes-vous sûr(e) de vouloir annuler ce rendez-vous ?
+        <IconButton
+          aria-label="close"
+          color="primary"
+          onClick={handleCloseModal}>
+          <CloseIcon fontSize="large" />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent dividers sx={{pt: 0, pb: 4, borderTop: 'none'}}>
+        <Typography variant="h5" gutterBottom>
+          Êtes-vous sûr(e) de vouloir annuler ce rendez-vous ?
         </Typography>
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          pt={2}
-          mx="auto"
-          width={{xs: '100%', md: '50%'}}>
-          <Button variant="outlined" onClick={handleCloseModal}>
-            Non
-          </Button>
-          <Button variant="contained" onClick={handleCancelAppointment}>
-            {loading ? <CircularProgress size={4} /> : 'Oui'}
-          </Button>
-        </Box>
-      </Box>
-    </Modal>
+        <Typography color="text.secondary">
+          {`Vous êtes sur le point d'annuler votre rendez-vous du ${formatDate(
+            appointment?.slotTime
+          )} avec le réparateur ${appointment.repairer.name}.`}
+        </Typography>
+      </DialogContent>
+      <DialogActions sx={{p: 2}}>
+        <Button variant="outlined" color="secondary" onClick={handleCloseModal}>
+          Annuler
+        </Button>
+        <Button
+          variant="contained"
+          color="error"
+          onClick={handleCancelAppointment}
+          startIcon={
+            loading && <CircularProgress size={18} sx={{color: 'white'}} />
+          }>
+          Valider
+        </Button>
+        {errorMessage && (
+          <Typography color="error" textAlign="center" sx={{pt: 4}}>
+            {errorMessage}
+          </Typography>
+        )}
+      </DialogActions>
+    </Dialog>
   );
 };
 
