cd37a167e96196d5c405d8963db478d16fde757a
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-07-27 10:18:35 +0200

    improve show bike modal design (#877)

--- pwa/components/dashboard/customers/ModalShowBike.tsx
+++ pwa/components/dashboard/customers/ModalShowBike.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import {Bike} from '@interfaces/Bike';
+import {
+  Dialog,
+  DialogTitle,
+  IconButton,
+  DialogContent,
+  Typography,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import PedalBikeIcon from '@mui/icons-material/PedalBike';
 import CopyrightIcon from '@mui/icons-material/Copyright';
 import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 import PersonIcon from '@mui/icons-material/Person';
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: '70%',
-  maxHeight: '100%',
-  overflow: 'scroll',
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+import {Bike} from '@interfaces/Bike';
 
 type ModalShowBikeProps = {
   bike: Bike | null;
@@ -38,92 +29,96 @@ const ModalShowBike = ({
   handleCloseModal,
 }: ModalShowBikeProps): JSX.Element => {
   return (
-    <Modal
+    <Dialog
+      maxWidth="sm"
+      fullWidth
       open={openModal}
       onClose={() => handleCloseModal()}
-      aria-labelledby="Show bike"
+      aria-labelledby="Affiche le vélo d'un utilisateur"
       aria-describedby="popup_show_bike">
-      <Box sx={style}>
-        <Box sx={{mt: 1}}>
-          {bike && (
-            <Box>
-              <List>
-                <ListItem>
-                  <ListItemIcon>
-                    <PedalBikeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary={bike.name} />
-                </ListItem>
-                <ListItem>
-                  <ListItemIcon>
-                    <PersonIcon />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={`${bike.owner.firstName} ${bike.owner.lastName}`}
-                  />
-                </ListItem>
-                {bike.brand && (
-                  <ListItem>
-                    <ListItemIcon>
-                      <CopyrightIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={bike.brand} />
-                  </ListItem>
-                )}
-                {bike.bikeType && (
-                  <ListItem>
-                    <ListItemIcon>
-                      <PedalBikeIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={bike.bikeType.name} />
-                  </ListItem>
-                )}
-                {bike.description && (
-                  <ListItem>
-                    <ListItemIcon>
-                      <TextSnippetIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={bike.description} />
-                  </ListItem>
-                )}
-
-                {bike.picture && (
-                  <img
-                    width="300"
-                    height="auto"
-                    alt="photo du véo"
-                    src={bike.picture.contentUrl}
-                  />
-                )}
-                {bike.wheelPicture && (
-                  <img
-                    width="300"
-                    height="auto"
-                    alt="photo de la roue"
-                    src={bike.wheelPicture.contentUrl}
-                  />
-                )}
-                {bike.transmissionPicture && (
-                  <img
-                    width="300"
-                    height="auto"
-                    alt="photo de la transmission"
-                    src={bike.transmissionPicture.contentUrl}
-                  />
-                )}
-              </List>
-
-              <Button
-                variant="outlined"
-                onClick={handleCloseModal}
-                sx={{float: 'right'}}>
-                Fermer
-              </Button>
-            </Box>
+      <DialogTitle
+        sx={{
+          m: 0,
+          p: 2,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Typography
+          id="modal-modal-title"
+          variant="h3"
+          color="primary"
+          textTransform="capitalize">
+          {bike?.name}
+        </Typography>
+        <IconButton
+          aria-label="close"
+          color="primary"
+          onClick={handleCloseModal}>
+          <CloseIcon fontSize="large" />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent dividers>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText
+              primary={`${bike?.owner.firstName} ${bike?.owner.lastName}`}
+            />
+          </ListItem>
+          {bike?.brand && (
+            <ListItem>
+              <ListItemIcon>
+                <CopyrightIcon />
+              </ListItemIcon>
+              <ListItemText primary={bike?.brand} />
+            </ListItem>
           )}
-        </Box>
-      </Box>
-    </Modal>
+          {bike?.bikeType && (
+            <ListItem>
+              <ListItemIcon>
+                <PedalBikeIcon />
+              </ListItemIcon>
+              <ListItemText primary={bike?.bikeType.name} />
+            </ListItem>
+          )}
+          {bike?.description && (
+            <ListItem>
+              <ListItemIcon>
+                <TextSnippetIcon />
+              </ListItemIcon>
+              <ListItemText primary={bike?.description} />
+            </ListItem>
+          )}
+          {bike?.picture && (
+            <img
+              width="300"
+              height="auto"
+              alt="photo du véo"
+              src={bike?.picture.contentUrl}
+            />
+          )}
+          {bike?.wheelPicture && (
+            <img
+              width="300"
+              height="auto"
+              alt="photo de la roue"
+              src={bike?.wheelPicture.contentUrl}
+            />
+          )}
+          {bike?.transmissionPicture && (
+            <img
+              width="300"
+              height="auto"
+              alt="photo de la transmission"
+              src={bike?.transmissionPicture.contentUrl}
+            />
+          )}
+        </List>
+      </DialogContent>
+    </Dialog>
   );
 };
 
