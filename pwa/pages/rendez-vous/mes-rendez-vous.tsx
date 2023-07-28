0cb8e4b1b78de91a3366f6b3a4b72a115dede58f
clementtalleu <clement.talleu@gmail.com>
2023-07-28 13:49:59 +0200

    fix typo

--- pwa/pages/rendez-vous/mes-rendez-vous.tsx
+++ pwa/pages/rendez-vous/mes-rendez-vous.tsx
@@ -38,7 +38,7 @@ const MyAppointments: NextPageWithLayout = () => {
               Connectez-vous pour accéder à la liste de vos rendez-vous
             </Typography>
             <Button variant="contained" onClick={handleLogin} sx={{mt: 4}}>
-              Me onnecter
+              Me connecter
             </Button>
           </Paper>
         )}
