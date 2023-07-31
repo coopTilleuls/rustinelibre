313ef4f8184d1eb26ddae37b985f1ed92c318c2d
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-07-31 16:39:47 +0200

    improve design of appointment create modal (#888)

--- pwa/components/dashboard/appointments/AppointmentCreateAddPhoto.tsx
+++ pwa/components/dashboard/appointments/AppointmentCreateAddPhoto.tsx
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import {mediaObjectResource} from '@resources/mediaObjectResource';
-import {CircularProgress, Typography, Stack, Box} from '@mui/material';
+import {CircularProgress, Typography, Box, useMediaQuery} from '@mui/material';
 import AddAPhotoIcon from '@mui/icons-material/AddAPhoto';
 import {uploadImage} from '@helpers/uploadFile';
-import {MediaObject} from '@interfaces/MediaObject';
 import {checkFileSize} from '@helpers/checkFileSize';
+import {MediaObject} from '@interfaces/MediaObject';
+import theme from 'styles/theme';
 
 interface AppointmentCreateAddPhotoProps {
   photo: MediaObject | null;
@@ -17,69 +18,84 @@ const AppointmentCreateAddPhoto = ({
 }: AppointmentCreateAddPhotoProps): JSX.Element => {
   const [loadingPhoto, setLoadingPhoto] = useState<boolean>(false);
   const [imageTooHeavy, setImageTooHeavy] = useState<boolean>(false);
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
   const handleFileChange = async (
     event: React.ChangeEvent<HTMLInputElement>
   ): Promise<void> => {
-    if (event.target.files) {
+    if (event.target.files && event.target.files.length > 0) {
       setImageTooHeavy(false);
       const file = event.target.files[0];
-
       if (!checkFileSize(file)) {
         setImageTooHeavy(true);
         return;
       }
-
       setLoadingPhoto(true);
       if (photo) {
         await mediaObjectResource.delete(photo['@id']);
       }
-
-      // Upload new picture
       const response = await uploadImage(file);
       const mediaObjectResponse = (await response?.json()) as MediaObject;
       if (mediaObjectResponse) {
-        // Display new photo
         setPhoto(mediaObjectResponse);
       }
-
       setLoadingPhoto(false);
     }
   };
 
   return (
-    <Stack
-      spacing={4}
-      display="flex"
-      flexDirection="column"
-      alignItems="center">
+    <Box display="flex" flexDirection="column" alignItems="center">
       {imageTooHeavy && (
         <Typography sx={{textAlign: 'center', color: 'red'}}>
           Votre photo dépasse la taille maximum autorisée (5mo)
         </Typography>
       )}
-      <Box border="1px solid grey" p={2} borderRadius={5}>
-        {loadingPhoto && <CircularProgress />}
+      <Box width="100%" boxShadow={2} borderRadius={6} my={1}>
+        {loadingPhoto && (
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            width={isMobile ? '100%' : '200'}
+            minHeight={200}
+            bgcolor="lightsecondary.main"
+            sx={{
+              borderRadius: 6,
+              boxShadow: 4,
+              overflow: 'hidden',
+            }}>
+            <CircularProgress sx={{fontSize: 60}} color="secondary" />
+          </Box>
+        )}
         {!loadingPhoto && (
           <label htmlFor="fileUpload">
-            {!photo ? (
-              <Box
-                sx={{cursor: 'pointer'}}
-                display="flex"
-                flexDirection="column"
-                alignItems="center">
-                <Typography component="p" sx={{mt: 2}}>
-                  Télécharger une photo
-                </Typography>
-                <AddAPhotoIcon sx={{fontSize: '3em'}} color="primary" />
-              </Box>
-            ) : (
-              <img
-                alt="Photo du diagnostic"
-                src={photo.contentUrl}
-                style={{cursor: 'pointer', width: '50%', marginLeft: '10%'}}
-              />
-            )}
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              width={isMobile ? '100%' : '200'}
+              minHeight={200}
+              bgcolor="lightsecondary.main"
+              sx={{
+                borderRadius: 6,
+                boxShadow: 4,
+                overflow: 'hidden',
+              }}>
+              {photo ? (
+                <img
+                  width="100%"
+                  src={photo.contentUrl}
+                  alt="Photo du diagnostic"
+                  style={{
+                    objectFit: 'cover',
+                    display: 'block',
+                    cursor: 'pointer',
+                  }}
+                />
+              ) : (
+                <AddAPhotoIcon sx={{fontSize: 60}} color="secondary" />
+              )}
+            </Box>
           </label>
         )}
         <input
@@ -90,7 +106,7 @@ const AppointmentCreateAddPhoto = ({
           onChange={(e) => handleFileChange(e)}
         />
       </Box>
-    </Stack>
+    </Box>
   );
 };
 
