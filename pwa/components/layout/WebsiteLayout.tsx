0e6416efb479657cbba15a6bbf42907710252cca
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-08-03 09:55:31 +0200

    fix bg color on admin login page (#908)

--- pwa/components/layout/WebsiteLayout.tsx
+++ pwa/components/layout/WebsiteLayout.tsx
@@ -6,6 +6,8 @@ import {useAccount} from '@contexts/AuthContext';
 import LegalNoticesFooter from './LegaNoticesFooter';
 import Box from '@mui/material/Box';
 import {useRouter} from 'next/router';
+import Logo from '@components/common/Logo';
+import {Typography} from '@mui/material';
 
 interface WebsiteLayoutProps extends PropsWithChildren {
   withLegalFooter?: boolean;
@@ -21,16 +23,35 @@ const WebsiteLayout = ({
     ? router.query.next.join('')
     : router.query.next || '/';
   const isAdmin = next.includes('admin');
+  const adminOnLoginPage = router.pathname === '/login' && isAdmin;
 
   return (
     <Box height="100vh" overflow="auto" id="websitelayout">
       {!isAdmin && <Navbar user={user ?? undefined} />}
       <Box
+        bgcolor={adminOnLoginPage ? 'primary.main' : ''}
         minHeight="100vh"
         display="flex"
         flexDirection="column"
         pt={{xs: '56px', sm: '64px', md: '80px'}}
         pb={{xs: '56px', md: '72px'}}>
+        {adminOnLoginPage && (
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            mx="auto"
+            maxWidth={400}>
+            <Logo inline color="lightprimary" />
+            <Typography
+              textAlign="center"
+              variant="caption"
+              fontWeight={600}
+              color="white">
+              ADMIN
+            </Typography>
+          </Box>
+        )}
         <Box flex={1}>{children}</Box>
         {!isAdmin && withLegalFooter && <LegalNoticesFooter />}
       </Box>
