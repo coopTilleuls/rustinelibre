0cf3944ebf918bf8544a26a2fd19f67fceb6b46c
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-08-03 09:47:08 +0200

    change 404 page design (#916)

--- pwa/pages/404.tsx
+++ pwa/pages/404.tsx
@@ -1,40 +1,63 @@
 import {NextPageWithLayout} from 'pages/_app';
 import React from 'react';
 import Head from 'next/head';
-import {Button, Container, Link, Typography, Stack} from '@mui/material';
+import {
+  Button,
+  Container,
+  Link,
+  Typography,
+  Stack,
+  Box,
+  useMediaQuery,
+} from '@mui/material';
 import WebsiteLayout from '@components/layout/WebsiteLayout';
+import Shape from '@components/common/Shape';
+import theme from 'styles/theme';
 
 const Error404: NextPageWithLayout = () => {
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
     <>
       <Head>
         <title>404</title>
       </Head>
       <WebsiteLayout>
-        <Container sx={{width: {xs: '100%', md: '70%'}}}>
-          <Stack
-            spacing={4}
-            pt={{xs: 4, md: 8}}
+        {/* <Container sx={{width: '100%', bgcolor: 'lightprimary.main'}}> */}
+        <Stack
+          sx={{
+            width: '100%',
+            minHeight: isMobile ? 400 : 500,
+            bgcolor: 'lightprimary.main',
+          }}
+          spacing={isMobile ? 6 : 8}
+          pt={{xs: 4, md: 8}}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          textAlign="center">
+          <Box
             display="flex"
-            flexDirection="column"
             alignItems="center"
-            textAlign="center">
-            <Typography fontSize={{xs: 28, md: 30}} fontWeight={600}>
+            gap={isMobile ? 4 : 8}
+            flexDirection={isMobile ? 'column' : 'row'}>
+            <Typography color="primary" variant="h2" component="h2">
               Désolé !
             </Typography>
-            <Typography>
-              Nous ne trouvons pas la page que vous demandez.
-            </Typography>
-            <Link href="/">
-              <Button
-                variant="contained"
-                sx={{textTransform: 'capitalize'}}
-                size="small">
-                Retourner sur l&apos;accueil
-              </Button>
-            </Link>
-          </Stack>
-        </Container>
+            <img
+              alt=""
+              src="/img/eclair.svg"
+              width={isMobile ? '40px' : '60px'}
+            />
+          </Box>
+          <Typography>
+            Nous ne trouvons pas la page que vous demandez.
+          </Typography>
+          <Link href="/">
+            <Button variant="contained">Retourner sur l&apos;accueil</Button>
+          </Link>
+        </Stack>
+        {/* </Container> */}
       </WebsiteLayout>
     </>
   );
