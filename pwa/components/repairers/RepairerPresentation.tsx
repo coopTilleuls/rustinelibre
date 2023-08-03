b25624722e93036a68b147f52c8a7417f1f86abc
AntoineC <78096284+AntoineCAU@users.noreply.github.com>
2023-08-03 09:55:59 +0200

    fix image presentation for a repairer into desk and mobile versions (#906)

--- pwa/components/repairers/RepairerPresentation.tsx
+++ pwa/components/repairers/RepairerPresentation.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
-import {Container, Box, Typography, Stack, Button} from '@mui/material';
-import {Repairer} from '@interfaces/Repairer';
 import RepairerPresentationCard from './RepairerPresentationCard';
+import {
+  Container,
+  Box,
+  Typography,
+  Stack,
+  Button,
+  useMediaQuery,
+} from '@mui/material';
+import {Repairer} from '@interfaces/Repairer';
+import theme from 'styles/theme';
+import RepairerPresentationImage from './RepairerPresentationImage';
 
 interface RepairerPresentationProps {
   repairer: Repairer;
@@ -11,6 +20,7 @@ interface RepairerPresentationProps {
 
 const RepairerPresentation = ({repairer}: RepairerPresentationProps) => {
   const {query} = useRouter();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
 
   let linkHref;
 
@@ -27,98 +37,90 @@ const RepairerPresentation = ({repairer}: RepairerPresentationProps) => {
   }
 
   return (
-    <>
-      <Box pt={4} pb={8} sx={{overflowX: 'clip'}}>
-        <Box
-          bgcolor="lightprimary.light"
-          height="100%"
-          width="100%"
-          position="absolute"
-          top="0"
-          left="0"
-          zIndex="-1"
-        />
-        <Container
+    <Box pt={4} pb={8} sx={{overflowX: 'clip'}}>
+      <Box
+        bgcolor="lightprimary.light"
+        height="100%"
+        width="100%"
+        position="absolute"
+        top="0"
+        left="0"
+        zIndex="-1"
+      />
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: {xs: 'column', md: 'row-reverse'},
+          alignItems: 'flex-start',
+          gap: 8,
+          maxWidth: '1000px!important',
+        }}>
+        <RepairerPresentationCard
+          repairer={repairer}
           sx={{
-            display: 'flex',
-            flexDirection: {xs: 'column', md: 'row-reverse'},
-            alignItems: 'flex-start',
-            gap: 8,
-            maxWidth: '1000px!important',
-          }}>
-          <RepairerPresentationCard
-            repairer={repairer}
-            sx={{
-              position: 'sticky',
-              width: '300px',
-              top: '112px',
-              display: {xs: 'none', md: 'flex'},
-            }}
-          />
-          <Stack spacing={4} flex={1} pt={2}>
-            <Box>
-              <Link href={linkHref}>
-                <Button variant="outlined" color="secondary" size="small">
-                  Retour
-                </Button>
-              </Link>
-              <Typography
-                component="h1"
-                variant="h1"
-                color="primary.main"
-                sx={{my: 1, textAlign: {xs: 'center', md: 'left'}}}>
-                {repairer.name}
-              </Typography>
-              <Typography
-                variant="h4"
-                sx={{textAlign: {xs: 'center', md: 'left'}}}>
-                {repairer.repairerType?.name}
-              </Typography>
-              <RepairerPresentationCard
-                repairer={repairer}
-                sx={{
-                  width: '100%',
-                  display: {xs: 'flex', md: 'none'},
-                  my: 3,
-                }}
-              />
-              {repairer.description && (
-                <Typography
-                  paragraph
-                  color="text.secondary"
-                  mt={3}
-                  dangerouslySetInnerHTML={{__html: repairer.description}}
-                />
-              )}
-            </Box>
-            {repairer.openingHours && (
-              <Box>
-                <Typography variant="h6" color="secondary.main">
-                  Horaires :
-                </Typography>
-                <Typography
-                  variant="body1"
-                  color="text.secondary"
-                  dangerouslySetInnerHTML={{__html: repairer.openingHours}}
-                />
-              </Box>
-            )}
-            {repairer.descriptionPicture ? (
-              <img
-                style={{
-                  maxWidth: '100%',
-                  height: 'auto',
-                  maxHeight: '350px',
-                  alignSelf: 'flex-start',
-                }}
-                src={repairer.descriptionPicture.contentUrl}
-                alt="Photo de description du réparateur"
-              />
+            position: 'sticky',
+            width: '300px',
+            top: '112px',
+            display: {xs: 'none', md: 'flex'},
+          }}
+        />
+        <Stack spacing={4} flex={1} pt={2}>
+          <Box>
+            <Link href={linkHref}>
+              <Button variant="outlined" color="secondary" size="small">
+                Retour
+              </Button>
+            </Link>
+            <Typography
+              component="h1"
+              variant="h1"
+              color="primary.main"
+              sx={{my: 1, textAlign: {xs: 'center', md: 'left'}}}>
+              {repairer.name}
+            </Typography>
+            <Typography
+              variant="h4"
+              sx={{textAlign: {xs: 'center', md: 'left'}}}>
+              {repairer.repairerType?.name}
+            </Typography>
+            {repairer.descriptionPicture && !isMobile ? (
+              <RepairerPresentationImage repairer={repairer} />
             ) : null}
-          </Stack>
-        </Container>
-      </Box>
-    </>
+            <RepairerPresentationCard
+              repairer={repairer}
+              sx={{
+                width: '100%',
+                display: {xs: 'flex', md: 'none'},
+                my: 3,
+              }}
+            />
+            {repairer.description && (
+              <Typography
+                paragraph
+                color="text.secondary"
+                mt={3}
+                dangerouslySetInnerHTML={{__html: repairer.description}}
+              />
+            )}
+          </Box>
+          {repairer.openingHours && (
+            <Box>
+              <Typography variant="h6" color="secondary.main">
+                Horaires :
+              </Typography>
+              <Typography
+                variant="body1"
+                color="text.secondary"
+                dangerouslySetInnerHTML={{__html: repairer.openingHours}}
+              />
+            </Box>
+          )}
+          {repairer.descriptionPicture && isMobile ? (
+            <RepairerPresentationImage repairer={repairer} />
+          ) : null}
+        </Stack>
+      </Container>
+    </Box>
   );
 };
 
