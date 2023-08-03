89dec129a52b4278c10040d9da1fd3f319b42253
Laury S <ginifizz@users.noreply.github.com>
2023-08-03 09:57:38 +0200

    feat: appointements design (#896)
    * feat: appointements design
    
    * fix: pr conflict

--- pwa/components/rendez-vous/MyFutureAppointments.tsx
+++ pwa/components/rendez-vous/MyFutureAppointments.tsx
@@ -5,30 +5,37 @@ import {appointmentResource} from '@resources/appointmentResource';
 import {Appointment} from '@interfaces/Appointment';
 import {dateObjectAsString} from '@helpers/dateHelper';
 import {AppointmentCard} from '@components/rendez-vous/AppointmentCard';
-import Grid from '@mui/material/Grid';
 
 interface MyFutureAppointmentProps {
   currentUser: User;
-  future: boolean;
+  setLoading: (b: boolean) => void;
 }
 
 const MyFutureAppointment = ({
   currentUser,
-  future,
+  setLoading,
 }: MyFutureAppointmentProps) => {
-  const [loading, setLoading] = useState<boolean>(false);
+  const [isEmpty, setEmpty] = useState<boolean | undefined>();
   const [appointments, setAppointments] = useState<Appointment[]>([]);
 
   const fetchAppointments = async () => {
     setLoading(true);
-    const response = await appointmentResource.getAll(true, {
+    const validated = await appointmentResource.getAll(true, {
       customer: currentUser.id,
       'order[slotTime]': 'ASC',
       'slotTime[after]': dateObjectAsString(new Date()),
       status: 'validated',
     });
+    const pending = await appointmentResource.getAll(true, {
+      customer: currentUser.id,
+      'order[slotTime]': 'ASC',
+      'slotTime[after]': dateObjectAsString(new Date()),
+      status: 'pending_repairer',
+    });
+    const response = [...validated['hydra:member'], ...pending['hydra:member']];
 
-    setAppointments(response['hydra:member']);
+    setAppointments(response);
+    setEmpty(!response?.length);
     setLoading(false);
   };
 
@@ -37,35 +44,36 @@ const MyFutureAppointment = ({
   }, [currentUser]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <Box>
-      <Typography variant="h4" sx={{mb: 2}}>
-        Rendez-vous à venir
-      </Typography>
-      <Box>
-        {!loading && appointments.length > 0 && (
-          <Grid container spacing={2}>
-            {!loading &&
-              appointments.map((appointment) => {
-                return (
-                  <Grid item xs={12} md={6} key={appointment.id}>
-                    <AppointmentCard
-                      appointment={appointment}
-                      future={future}
-                      fetchAppointments={fetchAppointments}
-                    />
-                  </Grid>
-                );
-              })}
-          </Grid>
-        )}
-        {!loading && appointments.length === 0 && (
-          <Box>
-            <Typography variant="body1">
-              Vous n&apos;avez pas de rendez vous à venir
-            </Typography>
-          </Box>
-        )}
+    <Box width="100%" textAlign="center">
+      <Box
+        sx={{
+          display: {
+            xs: 'flex',
+            md: 'grid',
+          },
+          width: '100%',
+          flexDirection: 'column',
+          maxWidth: '1200px',
+          mx: 'auto',
+          gap: 4,
+          gridTemplateColumns: {
+            md: 'repeat(auto-fit,minmax(400px,1fr))',
+          },
+          placeContent: 'center',
+        }}>
+        {appointments.map((appointment) => (
+          <AppointmentCard
+            key={appointment.id}
+            appointment={appointment}
+            fetchAppointments={fetchAppointments}
+          />
+        ))}
       </Box>
+      {isEmpty && (
+        <Typography sx={{my: 2}} variant="body1" color="text.secondary">
+          Vous n&apos;avez aucun rendez-vous à venir
+        </Typography>
+      )}
     </Box>
   );
 };
