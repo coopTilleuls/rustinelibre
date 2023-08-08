e2903a66765f20e4a2e51de45a60e41230ed46b4
Davy <davy.beauzil72@gmail.com>
2023-08-08 13:13:58 +0200

    feat: remove automatically unused photo in AutoDiagnostic

---
+++ api/src/AutoDiagnostics/EventSubscriber/RemoveMediaObjectEventSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AutoDiagnostics\EventSubscriber;
+
+use ApiPlatform\Symfony\EventListener\EventPriorities;
+use App\Entity\AutoDiagnostic;
+use App\Repository\MediaObjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+readonly class RemoveMediaObjectEventSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private MediaObjectRepository $mediaObjectRepository
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::VIEW => ['removeMediaObject', EventPriorities::PRE_WRITE],
+        ];
+    }
+
+    public function removeMediaObject(ViewEvent $event): void
+    {
+        $object = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$object instanceof AutoDiagnostic || Request::METHOD_PUT !== $method) {
+            return;
+        }
+
+        $originalEntity = $this->entityManager->getUnitOfWork()->getOriginalEntityData($object);
+
+        if (array_key_exists('photo_id', $originalEntity) && $object->photo && $originalEntity['photo_id'] !== $object->photo->id) {
+            $this->mediaObjectRepository->remove($this->mediaObjectRepository->find($originalEntity['photo_id']));
+        }
+    }
+}
