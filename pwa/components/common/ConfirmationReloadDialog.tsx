c7f2c621c6406f777dcb91c571e60d0713210de2
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-09-12 10:35:22 +0200

    adding confirmation when reload search repairer page

---
+++ pwa/components/common/ConfirmationReloadDialog.tsx
@@ -0,0 +1,20 @@
+import {useEffect} from 'react';
+
+const ConfirmationReloadDialog = () => {
+  useEffect(() => {
+    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+      e.preventDefault();
+      e.returnValue = '';
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+
+  return null;
+};
+
+export default ConfirmationReloadDialog;
