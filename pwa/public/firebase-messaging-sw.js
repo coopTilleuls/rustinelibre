b93054df6c9ded256619f8eeb7fddfa3ecf272ba
Antoine Cau <antoine.cau@les-tilleuls.coop>
2023-10-18 09:27:54 +0200

    trying worker fix v2

--- pwa/public/firebase-messaging-sw.js
+++ pwa/public/firebase-messaging-sw.js
@@ -5,114 +5,6 @@ importScripts(
   'https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js'
 );
 
-/**
- * Copyright 2018 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// If the loader is already loaded, just stop.
-if (!self.define) {
-  let registry = {};
-
-  // Used for `eval` and `importScripts` where we can't get script URL by other means.
-  // In both cases, it's safe to use a global var because those functions are synchronous.
-  let nextDefineUri;
-
-  const singleRequire = (uri, parentUri) => {
-    uri = new URL(uri + '.js', parentUri).href;
-    return (
-      registry[uri] ||
-      new Promise((resolve) => {
-        if ('document' in self) {
-          const script = document.createElement('script');
-          script.src = uri;
-          script.onload = resolve;
-          document.head.appendChild(script);
-        } else {
-          nextDefineUri = uri;
-          importScripts(uri);
-          resolve();
-        }
-      }).then(() => {
-        let promise = registry[uri];
-        if (!promise) {
-          throw new Error(`Module ${uri} didn’t register its module`);
-        }
-        return promise;
-      })
-    );
-  };
-
-  self.define = (depsNames, factory) => {
-    const uri =
-      nextDefineUri ||
-      ('document' in self ? document.currentScript.src : '') ||
-      location.href;
-    if (registry[uri]) {
-      // Module is already loading or loaded.
-      return;
-    }
-    let exports = {};
-    const require = (depUri) => singleRequire(depUri, uri);
-    const specialDeps = {
-      module: {uri},
-      exports,
-      require,
-    };
-    registry[uri] = Promise.all(
-      depsNames.map((depName) => specialDeps[depName] || require(depName))
-    ).then((deps) => {
-      factory(...deps);
-      return exports;
-    });
-  };
-}
-define(['./workbox-3576cac3'], function (workbox) {
-  'use strict';
-
-  importScripts();
-  self.skipWaiting();
-  workbox.clientsClaim();
-  workbox.registerRoute(
-    '/',
-    new workbox.NetworkFirst({
-      cacheName: 'start-url',
-      plugins: [
-        {
-          cacheWillUpdate: async ({request, response, event, state}) => {
-            if (response && response.type === 'opaqueredirect') {
-              return new Response(response.body, {
-                status: 200,
-                statusText: 'OK',
-                headers: response.headers,
-              });
-            }
-            return response;
-          },
-        },
-      ],
-    }),
-    'GET'
-  );
-  workbox.registerRoute(
-    /.*/i,
-    new workbox.NetworkOnly({
-      cacheName: 'dev',
-      plugins: [],
-    }),
-    'GET'
-  );
-});
-//# sourceMappingURL=sw.js.map
-
 firebase.initializeApp(
   JSON.parse(new URL(location).searchParams.get('firebaseConfig'))
 );
@@ -134,7 +26,7 @@ messaging.onBackgroundMessage((payload) => {
       payload.data.image ||
       'https://cdn-icons-png.flaticon.com/512/565/565422.png',
     data: {
-      url: payload.data.route ?? '/',
+      url: payload.data.route || '/',
     },
     vibrate: [200, 100, 200],
   };
@@ -159,4 +51,96 @@ self.addEventListener('notificationclick', function (event) {
   if (clients.openWindow && url) {
     event.waitUntil(clients.openWindow(url));
   }
+});
+
+// Définition de "define" pour l'import de Workbox
+if (!self.define) {
+  let registry = {};
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + '.js', parentUri).href;
+    return (
+      registry[uri] ||
+      new Promise((resolve) => {
+        if ('document' in self) {
+          const script = document.createElement('script');
+          script.src = uri;
+          script.onload = resolve;
+          document.head.appendChild(script);
+        } else {
+          importScripts(uri);
+          resolve();
+        }
+      }).then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri =
+      ('document' in self ? document.currentScript.src : '') || location.href;
+    if (registry[uri]) {
+      return;
+    }
+    let exports = {};
+    const require = (depUri) => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: {uri},
+      exports,
+      require,
+    };
+    registry[uri] = Promise.all(
+      depsNames.map((depName) => specialDeps[depName] || require(depName))
+    ).then((deps) => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+
+// Import de Workbox
+define(['./workbox-3576cac3'], function (workbox) {
+  'use strict';
+
+  workbox.clientsClaim();
+  workbox.skipWaiting();
+
+  // ...
+
+  // Stratégies de mise en cache avec Workbox
+  workbox.registerRoute(
+    '/',
+    new workbox.NetworkFirst({
+      cacheName: 'start-url',
+      plugins: [
+        {
+          cacheWillUpdate: async ({request, response, event, state}) => {
+            if (response && response.type === 'opaqueredirect') {
+              return new Response(response.body, {
+                status: 200,
+                statusText: 'OK',
+                headers: response.headers,
+              });
+            }
+            return response;
+          },
+        },
+      ],
+    }),
+    'GET'
+  );
+
+  workbox.registerRoute(
+    /.*/i,
+    new workbox.NetworkOnly({
+      cacheName: 'dev',
+      plugins: [],
+    }),
+    'GET'
+  );
 });
