92a19954ad9b7cda702e45fc54370681cf6f66f4
Davy <davy.beauzil72@gmail.com>
2024-03-01 10:45:32 +0100

    fix: App\Tests\Repairer\CreateUserAndRepairerTest::testPostRepairerAndUserMissingFields

--- api/tests/Repairer/CreateUserAndRepairerTest.php
+++ api/tests/Repairer/CreateUserAndRepairerTest.php
@@ -50,7 +50,7 @@ class CreateUserAndRepairerTest extends AbstractTestCase
             'headers' => ['Content-Type' => 'application/json'],
             'json' => $jsonMissingFields,
         ]);
-        $this->assertResponseStatusCodeSame(400);
+        $this->assertResponseStatusCodeSame(422);
     }
 
     public function testPostRepairerAndUser(): void
